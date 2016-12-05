393c1db9fa07d4cc3408565deec8375097f3f233
Patrick Armstrong <parmstrong@WS01007.local>
2016-12-05 12:03:49 +0000

    Adds AppHeader component

---
+++ src/AppHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AppHeader.css';
+
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <div className="AppHeader">
+        <h1>Honesty Store</h1>
+      </div>
+    );
+  }
+}
+
+export default AppHeader;
