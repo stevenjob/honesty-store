29116e697b2663a854af088aaabac9c1292d6508
Sam Burnstone <sburnstone@scottlogic.com>
2016-12-05 18:30:15 +0000

    Add basic button and arrange in grid

--- src/store-screen/Product.js
+++ src/store-screen/Product.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { ListGroupItem } from 'react-bootstrap';
+import { ListGroupItem, Button, Grid, Col, Row } from 'react-bootstrap';
 
 class Product extends React.Component {
 
   render() {
     return (
-      <ListGroupItem>{this.props.name} - £{this.props.price.toFixed(2)}</ListGroupItem>
+      <ListGroupItem>
+        <Grid>
+          <Row>
+            <Col xs={9}>
+              <p>{this.props.name} - £{this.props.price.toFixed(2)}</p>
+            </Col>
+            <Col xs={3}>
+              <Button bsStyle="success" bsSize="small">Buy</Button>
+            </Col>
+          </Row>
+        </Grid>
+      </ListGroupItem>
     )
   }
 }
