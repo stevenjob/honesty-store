035446911b43b8347f124ddd62389f05f1bf9281
Patrick Armstrong <parmstrong@WS01007.local>
2016-12-06 15:21:22 +0000

    Renames Store to StoreScreen

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router'
 import App from './App';
 import BuyScreen from './buy-screen/BuyScreen';
-import Store from './store-screen/Store';
+import StoreScreen from './store-screen/StoreScreen';
 import './index.css';
 
 ReactDOM.render((
@@ -11,7 +11,7 @@ ReactDOM.render((
     <Route path="/" component={App}>
       // TODO this should be a link from the Buy buttons, not a route in here
       <Route path="/buy" component={BuyScreen}/>
-      <Route path="/:name" component={Store}/>
+      <Route path="/:name" component={StoreScreen}/>
     </Route>
   </Router>
 ), document.getElementById('root'));
