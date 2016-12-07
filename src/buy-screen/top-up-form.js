8d5fc26c54182e2d6a491e0ba9430126a655b0ed
Sam Burnstone <sburnstone@scottlogic.com>
2016-12-07 16:43:23 +0000

    Alter top up form to handle changes made to input fields

--- src/buy-screen/top-up-form.js
+++ src/buy-screen/top-up-form.js
@@ -3,6 +3,31 @@ import { FormGroup, ControlLabel, FormControl, Button, Radio } from 'react-boots
 
 class TopUpForm extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardNumber: '',
+      expiryDate: '',
+      cvcNumber: ''
+    };
+
+    this.handleCardNumberChange = this.handleCardNumberChange.bind(this);
+    this.handleExpiryDateChange = this.handleExpiryDateChange.bind(this);
+    this.handleCVCNumberChange = this.handleCVCNumberChange.bind(this);
+  }
+
+  handleCardNumberChange(event) {
+    this.setState({cardNumber: event.target.value});
+  }
+
+  handleExpiryDateChange(event) {
+    this.setState({expiryDate: event.target.value});
+  }
+
+  handleCVCNumberChange(event) {
+    this.setState({cvcNumber: event.target.value});
+  }
+
   render() {
     return (
       <div>
@@ -15,19 +40,28 @@ class TopUpForm extends React.Component {
             <ControlLabel>Card Number</ControlLabel>
             <FormControl
               type="text"
-              placeholder="0000000000000000" />
+              value={this.state.cardNumber}
+              placeholder="0000000000000000"
+              onChange={this.handleCardNumberChange}
+            />
           </FormGroup>
           <FormGroup>
             <ControlLabel>Expiry Date</ControlLabel>
             <FormControl
               type="text"
-              placeholder="MM/YY" />
+              value={this.state.expiryDate}
+              placeholder="MM/YY"
+              onChange={this.handleExpiryDateChange}
+            />
           </FormGroup>
           <FormGroup>
             <ControlLabel>CVC</ControlLabel>
             <FormControl
               type="text"
-              placeholder="123" />
+              value={this.state.cvcNumber}
+              placeholder="123"
+              onChange={this.handleCVCNumberChange}
+            />
           </FormGroup>
           <Button type="submit">Top Up</Button>
         </form>
