1bcf5ffb68e40a0d392f8873f9f48f6313f2630b
Patrick Armstrong <parmstrong@WS01007.local>
2016-12-08 10:31:25 +0000

    Adds Buy button to BuyScreen
    In addition this uses its own state to decide if the user is signed up or not. If not, the SignUpForm is shown but the TopUpForm is not. The Buy button is grey and deactivated. If the user is signed up it shows a zero balance, displays the TopUpForm and activates and makes green the Buy button.
    
    No way currently to change the state.

--- src/buy-screen/buy-screen.js
+++ src/buy-screen/buy-screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
 import SignUpForm from './sign-up-form';
 import TopUpForm from './top-up-form';
 import ChosenProduct from './chosen-product';
@@ -7,26 +7,46 @@ import store from '../product-store';
 
 class BuyScreen extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {isSignedUp: false};
+  }
+
   render() {
+    const isSignedUp = this.state.isSignedUp;
+    const buyButtonStyle = isSignedUp ? "success" : "default";
     const chosenProductID = Number(this.props.params.productId);
     const chosenProduct = store.getProduct(chosenProductID);
+
     return (
       <Grid>
+
         <Row>
           <Col xs={12}>
-            <SignUpForm />
+            {isSignedUp ? <p>Balance: £0.00</p> : <SignUpForm />}
           </Col>
         </Row>
-        <Row>
-          <Col xs={12}>
-            <TopUpForm />
-          </Col>
-        </Row>
+
+        {isSignedUp &&
+          <Row>
+            <Col xs={12}>
+              <TopUpForm />
+            </Col>
+          </Row>
+        }
+
         <Row>
           <Col xs={12}>
             <ChosenProduct product={chosenProduct}/>
           </Col>
         </Row>
+
+        <Row>
+          <Col xs={12}>
+            <Button bsStyle={buyButtonStyle} disabled={!isSignedUp}>Buy</Button>
+          </Col>
+        </Row>
+
       </Grid>
     );
   }
