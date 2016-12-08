3c2f2515a9729ef1aecf122cf801a42a5ffac107
Robert Pilling <rpilling@scottlogic.co.uk>
2016-12-08 09:56:50 +0000

    read credentials from the environment by default
    http://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-environment.html

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import * as program from 'commander';
 import { config } from 'aws-sdk';
 import ecrDeploy from './ecr/deploy';
 
-config.credentials = {
-  accessKeyId: "AKIAIBP2ZVIV4UKIWZQQ",
-  secretAccessKey: "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"
-};
 config.region = "eu-west-1";
 
 const warnAndExit = e => {
