a7c4ad93fcd9c845607fa04ed1dc30488465ec6f
Sam Burnstone <sburnstone@scottlogic.com>
2016-12-16 14:10:21 +0000

    Clean up app.js

--- api/app.js
+++ api/app.js
@@ -10,14 +10,9 @@ app.use(bodyParser.json());
 /** Controller Configuration **/
 setupRegisterPhases(router);
 
-/** Routes **/
-
 // All routes must be prefixed with '/api/v1'
 app.use('/api/v1', router);
 
-/** Port Listening **/
-app.listen(3000, () => {
-  console.log('Honesty store API app is listening on port 3000');
-});
+app.listen(3000);
 
 module.exports = app;
