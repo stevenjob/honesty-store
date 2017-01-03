942cb32b72e23bda70af61735894e938b5c0ce97
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-03 18:31:56 +0000

    Hook up session controller to router

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const registerController = require('./controllers/register');
+const sessionController = require('./controllers/session');
 
 const app = express();
 const router = express.Router();
@@ -8,6 +9,7 @@ const router = express.Router();
 app.use(bodyParser.json());
 
 registerController(router);
+sessionController(router);
 
 app.use('/api/v1', router);
 
