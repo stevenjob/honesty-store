999d8d0823b9c446f3216e9f4dcef9855703dfe9
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-03 18:28:02 +0000

    Add session controller

---
+++ api/controllers/session.js
@@ -0,0 +1,42 @@
+const HTTPStatus = require('http-status');
+const { authenticateRefreshToken } = require('../middleware/authenticate');
+const { getCardNumber, getDefaultStoreCode, getAccessToken } = require('../services/accounts');
+const { getTransactionHistory, getBalance } = require('../services/transactions');
+const { getItems } = require('../services/store');
+
+const maskCardNumber = cardNumber => cardNumber.replace(/\d(?=\d{4})/g, 'X');
+
+const getUserSessionData = (userID) => {
+  const cardNumber = getCardNumber(userID) || '';
+  const maskedCardNumber = maskCardNumber(cardNumber);
+  return {
+    balance: getBalance(userID),
+    transactions: getTransactionHistory(userID),
+    cardNumber: maskedCardNumber,
+  };
+};
+
+const getStoreSessionData = (userID) => {
+  const connectedStoreCode = getDefaultStoreCode(userID);
+  return {
+    items: getItems(connectedStoreCode),
+    id: connectedStoreCode,
+  };
+};
+
+const setupSessionEndpoint = (router) => {
+  router.post(
+    '/session',
+    authenticateRefreshToken,
+    (request, response) => {
+      response.status(HTTPStatus.OK).json({
+        response: {
+          user: getUserSessionData(request.accountID),
+          store: getStoreSessionData(request.accountID),
+          accessToken: getAccessToken(request.accountID),
+        },
+      });
+    });
+};
+
+module.exports = setupSessionEndpoint;
---
+++ api/test/session-controller.js
@@ -0,0 +1,164 @@
+const request = require('request');
+const assert = require('chai').assert;
+const HTTPStatus = require('http-status');
+const { registerAccount, updateAccount, __accounts } = require('../services/accounts');
+const { addItemTransaction } = require('../services/transactions');
+const { getItems } = require('../services/store');
+
+require('../app');
+
+const baseURL = 'http://localhost:3000/api/v1';
+
+describe('/session', () => {
+  beforeEach(() => {
+    __accounts.length = 0;
+  });
+
+  describe('Refresh Token Validation', () => {
+    it('should return response with \'UNAUTHORIZED\' status code when invalid refresh token provided',
+      (done) => {
+        request.post(`${baseURL}/session`)
+          .auth(null, null, true, '123')
+          .on('response', (response) => {
+            assert.equal(response.statusCode, HTTPStatus.UNAUTHORIZED);
+            done();
+          });
+      });
+    it('should return response with \'OK\' status code when valid refresh token provided',
+      (done) => {
+        const { refreshToken } = registerAccount('NCL');
+        request.post(`${baseURL}/session`)
+          .auth(null, null, true, refreshToken)
+          .on('response', (response) => {
+            assert.equal(response.statusCode, HTTPStatus.OK);
+            done();
+          });
+      });
+  });
+
+  describe('Response Data', () => {
+    it('should return masked user card number',
+      (done) => {
+        const account = registerAccount('NCL');
+        const cardDetails = '1234123412341234';
+        updateAccount(account.id, '', cardDetails);
+
+        request.post({
+          uri: `${baseURL}/session`,
+          auth: {
+            bearer: account.refreshToken,
+          },
+          json: true,
+        },
+        (error, response, body) => {
+          const expectedCardNumber = 'XXXXXXXXXXXX1234';
+          assert.equal(body.response.user.cardNumber, expectedCardNumber);
+          done();
+        });
+      });
+
+    it('should return user transcation details',
+      (done) => {
+        const account = registerAccount('NCL');
+        const itemPrices = [20, 50];
+
+        addItemTransaction(account.id, itemPrices[0]);
+        addItemTransaction(account.id, itemPrices[1]);
+
+        request.post({
+          uri: `${baseURL}/session`,
+          auth: {
+            bearer: account.refreshToken,
+          },
+          json: true,
+        },
+        (error, response, body) => {
+          const transactions = body.response.user.transactions;
+          assert.equal(transactions.length, itemPrices.length);
+          assert.equal(transactions[0].amount, -itemPrices[0]);
+          assert.equal(transactions[1].amount, -itemPrices[1]);
+          done();
+        });
+      });
+
+    it('should return user\'s balance',
+      (done) => {
+        const account = registerAccount('NCL');
+        const itemPrice = 100;
+
+        addItemTransaction(account.id, itemPrice);
+
+        request.post({
+          uri: `${baseURL}/session`,
+          auth: {
+            bearer: account.refreshToken,
+          },
+          json: true,
+        },
+        (error, response, body) => {
+          const balance = body.response.user.balance;
+          assert.equal(balance, -itemPrice);
+          done();
+        });
+      });
+
+    it('should return store items',
+      (done) => {
+        const storeCode = 'NCL';
+        const account = registerAccount(storeCode);
+
+        request.post({
+          uri: `${baseURL}/session`,
+          auth: {
+            bearer: account.refreshToken,
+          },
+          json: true,
+        },
+        (error, response, body) => {
+          const expectedItems = getItems(storeCode);
+          assert.deepEqual(body.response.store.items, expectedItems);
+          done();
+        });
+      });
+
+    it('should return user\'s default store ID',
+      (done) => {
+        const storeCode = 'NCL';
+        // When registering, we set the user's current store to be 'storeCode'
+        const account = registerAccount(storeCode);
+
+        request.post({
+          uri: `${baseURL}/session`,
+          auth: {
+            bearer: account.refreshToken,
+          },
+          json: true,
+        },
+        (error, response, body) => {
+          assert.equal(body.response.store.id, storeCode);
+          done();
+        });
+      });
+
+    it('should return a new access token',
+      (done) => {
+        const storeCode = 'NCL';
+        // When registering, we set the user's current store to be 'storeCode'
+        const account = registerAccount(storeCode);
+
+        const previousAccessToken = account.accessToken;
+
+        request.post({
+          uri: `${baseURL}/session`,
+          auth: {
+            bearer: account.refreshToken,
+          },
+          json: true,
+        },
+        (error, response, body) => {
+          assert.notEqual(body.response.accessToken, previousAccessToken);
+          done();
+        });
+      });
+  });
+});
