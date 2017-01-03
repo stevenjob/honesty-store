79302ed99befc6f4e65bccc2bed72e2cab258dc4
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-03 18:27:47 +0000

    Add ability to authenticate both access and refresh tokens

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -1,49 +1,56 @@
 const jwt = require('jsonwebtoken');
 const HTTPStatus = require('http-status');
 const { secretKey } = require('../constants');
-const { getAccountID } = require('../services/accounts');
+const { getAccountIDFromAccessToken, getAccountIDFromRefreshToken } = require('../services/accounts');
 
-const authenticate = (request, response, next) => {
-  // This comes in the form: 'Bearer <token>'
-  const token = request.headers.authorization.split(' ')[1];
+const getToken = request => request.headers.authorization.split(' ')[1];
 
-  if (token == null || token === '') {
+const tokenIsValid = (token) => {
+  let isValid = true;
+  jwt.verify(token, secretKey, (err) => {
+    if (err) {
+      isValid = false;
+    }
+  });
+  return isValid;
+};
+
+const authenticateToken = (request, response, next, tokenRetrievalGetter) => {
+  const sendInvalidTokenResponse = () => {
     response.status(HTTPStatus.UNAUTHORIZED)
       .json({
         error: {
-          message: 'No access token provided',
+          message: 'Invalid token provided',
         },
       });
+  };
+
+  const token = getToken(request);
+
+  if (!tokenIsValid(token)) {
+    sendInvalidTokenResponse();
     return;
   }
 
-  jwt.verify(token, secretKey, (err) => {
-    if (err) {
-      response.status(HTTPStatus.UNAUTHORIZED)
-        .json({
-          error: {
-            message: 'Invalid access token provided',
-          },
-        });
-      return;
-    }
+  // Check valid account
+  let accountID;
 
-    // Check valid account
-    const accountID = getAccountID(token);
-    if (accountID == null) {
-      response.status(HTTPStatus.UNAUTHORIZED)
-        .json({
-          error: {
-            message: 'Invalid access token provided',
-          },
-        });
-      return;
-    }
+  try {
+    accountID = tokenRetrievalGetter(token);
+  } catch (e) {
+    sendInvalidTokenResponse();
+    return;
+  }
 
-    // eslint-disable-next-line no-param-reassign
-    request.accountID = accountID;
-    next();
-  });
+  // eslint-disable-next-line no-param-reassign
+  request.accountID = accountID;
+  next();
 };
 
-module.exports = authenticate;
+const authenticateAccessToken = (request, response, next) =>
+  authenticateToken(request, response, next, getAccountIDFromAccessToken);
+
+const authenticateRefreshToken = (request, response, next) =>
+  authenticateToken(request, response, next, getAccountIDFromRefreshToken);
+
+module.exports = { authenticateAccessToken, authenticateRefreshToken };
