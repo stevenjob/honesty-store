1090f38359c774620f5681ef0d5c668e56760632
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-04 16:49:04 +0000

    Add tests to ensure register endpoints return session data

--- api/test/register-controller.js
+++ api/test/register-controller.js
@@ -1,7 +1,8 @@
 const request = require('request');
 const assert = require('chai').assert;
 const HTTPStatus = require('http-status');
-const { registerAccount, __accounts } = require('../services/accounts');
+const { registerAccount, __accounts, getAccountIDFromAccessToken } = require('../services/accounts');
+const getSessionData = require('../services/session');
 
 require('../app');
 
@@ -12,7 +13,7 @@ describe('/register', () => {
     __accounts.length = 0;
   });
 
-  describe('Token response data', () => {
+  describe('Response Data', () => {
     const sendRequest = (storeCode, callback) => {
       const options = {
         uri: `${baseURL}/register`,
@@ -40,10 +41,50 @@ describe('/register', () => {
           done();
         });
     });
+
+    it('should contain session data as part of its JSON response', (done) => {
+      const storeCode = 'NCL';
+      sendRequest(storeCode,
+        (error, response, body) => {
+          const userID = getAccountIDFromAccessToken(body.response.accessToken);
+          const expectedSessionData = getSessionData(userID);
+          assert.deepEqual(body.response.user, expectedSessionData.user);
+          assert.deepEqual(body.response.store, expectedSessionData.store);
+          done();
+        });
+    });
   });
 });
 
 describe('/register2', () => {
+  const sendRequest = (attributes, callback) => {
+    const { accessToken } = registerAccount('NCL');
+    request.post({
+      uri: `${baseURL}/register2`,
+      auth: {
+        bearer: accessToken,
+      },
+      json: true,
+      body: attributes,
+    },
+    (error, response, body) => {
+      callback(response, body, accessToken);
+    });
+  };
+
+  describe('Response Data', () => {
+    it('should contain session data as part of its JSON response', (done) => {
+      sendRequest({ emailAddress: 'test@test.co.uk' },
+        (response, body, accessToken) => {
+          const userID = getAccountIDFromAccessToken(accessToken);
+          const expectedSessionData = getSessionData(userID);
+          assert.deepEqual(body.response.user, expectedSessionData.user);
+          assert.deepEqual(body.response.store, expectedSessionData.store);
+          done();
+        });
+    });
+  });
+
   describe('Token validation', () => {
     it('should return \'UNAUTHORIZED\' status code when no token provided', (done) => {
       request.post(`${baseURL}/register2`)
@@ -63,21 +104,6 @@ describe('/register2', () => {
         });
     });
   });
-
-  const sendRequest = (attributes, callback) => {
-    const { accessToken } = registerAccount('NCL');
-    request.post({
-      uri: `${baseURL}/register2`,
-      auth: {
-        bearer: accessToken,
-      },
-      json: true,
-      body: attributes,
-    },
-    (error, response, body) => {
-      callback(response, body);
-    });
-  };
 
   describe('Email validation', () => {
     it('should return \'UNAUTHORIZED\' status code and relevant message when no email address provided', (done) => {
