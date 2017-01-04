06e9142c7f55de9505e352ace70f1a02c88dfe7b
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-04 16:26:14 +0000

    Move session data tests into session service test
    We will add a new test to the '/session' suite to ensure the
    session controller uses the service and outputs the data in its
    JSON response.

--- api/test/session-controller.js
+++ api/test/session-controller.js
@@ -1,9 +1,7 @@
 const request = require('request');
 const assert = require('chai').assert;
 const HTTPStatus = require('http-status');
-const { registerAccount, updateAccount, __accounts } = require('../services/accounts');
-const { addItemTransaction } = require('../services/transactions');
-const { getItems } = require('../services/store');
+const { registerAccount, __accounts } = require('../services/accounts');
 
 require('../app');
 
@@ -37,109 +35,6 @@ describe('/session', () => {
   });
 
   describe('Response Data', () => {
-    it('should return masked user card number',
-      (done) => {
-        const account = registerAccount('NCL');
-        const cardDetails = '1234123412341234';
-        updateAccount(account.id, '', cardDetails);
-
-        request.post({
-          uri: `${baseURL}/session`,
-          auth: {
-            bearer: account.refreshToken,
-          },
-          json: true,
-        },
-        (error, response, body) => {
-          const expectedCardNumber = 'XXXXXXXXXXXX1234';
-          assert.equal(body.response.user.cardNumber, expectedCardNumber);
-          done();
-        });
-      });
-
-    it('should return user transcation details',
-      (done) => {
-        const account = registerAccount('NCL');
-        const itemPrices = [20, 50];
-
-        addItemTransaction(account.id, itemPrices[0]);
-        addItemTransaction(account.id, itemPrices[1]);
-
-        request.post({
-          uri: `${baseURL}/session`,
-          auth: {
-            bearer: account.refreshToken,
-          },
-          json: true,
-        },
-        (error, response, body) => {
-          const transactions = body.response.user.transactions;
-          assert.equal(transactions.length, itemPrices.length);
-          assert.equal(transactions[0].amount, -itemPrices[0]);
-          assert.equal(transactions[1].amount, -itemPrices[1]);
-          done();
-        });
-      });
-
-    it('should return user\'s balance',
-      (done) => {
-        const account = registerAccount('NCL');
-        const itemPrice = 100;
-
-        addItemTransaction(account.id, itemPrice);
-
-        request.post({
-          uri: `${baseURL}/session`,
-          auth: {
-            bearer: account.refreshToken,
-          },
-          json: true,
-        },
-        (error, response, body) => {
-          const balance = body.response.user.balance;
-          assert.equal(balance, -itemPrice);
-          done();
-        });
-      });
-
-    it('should return store items',
-      (done) => {
-        const storeCode = 'NCL';
-        const account = registerAccount(storeCode);
-
-        request.post({
-          uri: `${baseURL}/session`,
-          auth: {
-            bearer: account.refreshToken,
-          },
-          json: true,
-        },
-        (error, response, body) => {
-          const expectedItems = getItems(storeCode);
-          assert.deepEqual(body.response.store.items, expectedItems);
-          done();
-        });
-      });
-
-    it('should return user\'s default Store Code',
-      (done) => {
-        const storeCode = 'NCL';
-        // When registering, we set the user's current store to be 'storeCode'
-        const account = registerAccount(storeCode);
-
-        request.post({
-          uri: `${baseURL}/session`,
-          auth: {
-            bearer: account.refreshToken,
-          },
-          json: true,
-        },
-        (error, response, body) => {
-          assert.equal(body.response.store.code, storeCode);
-          done();
-        });
-      });
-
     it('should return a new access token',
       (done) => {
         const storeCode = 'NCL';
---
+++ api/test/session-service.js
@@ -0,0 +1,74 @@
+const assert = require('chai').assert;
+const { registerAccount, updateAccount, __accounts } = require('../services/accounts');
+const { addItemTransaction } = require('../services/transactions');
+const { getItems } = require('../services/store');
+const getSessionData = require('../services/session');
+
+describe('Session Service', () => {
+  beforeEach(() => {
+    __accounts.length = 0;
+  });
+
+  it('should return masked user card number',
+    () => {
+      const account = registerAccount('NCL');
+      const cardDetails = '1234123412341234';
+      updateAccount(account.id, '', cardDetails);
+
+      const sessionData = getSessionData(account.id);
+
+      const expectedCardNumber = 'XXXXXXXXXXXX1234';
+      assert.equal(sessionData.user.cardNumber, expectedCardNumber);
+    });
+
+  it('should return user transcation details',
+    () => {
+      const account = registerAccount('NCL');
+      const itemPrices = [20, 50];
+
+      addItemTransaction(account.id, itemPrices[0]);
+      addItemTransaction(account.id, itemPrices[1]);
+
+      const sessionData = getSessionData(account.id);
+
+      const transactions = sessionData.user.transactions;
+      assert.equal(transactions.length, itemPrices.length);
+      assert.equal(transactions[0].amount, -itemPrices[0]);
+      assert.equal(transactions[1].amount, -itemPrices[1]);
+    });
+
+  it('should return user\'s balance',
+    () => {
+      const account = registerAccount('NCL');
+      const itemPrice = 100;
+
+      addItemTransaction(account.id, itemPrice);
+
+      const sessionData = getSessionData(account.id);
+
+      const balance = sessionData.user.balance;
+      assert.equal(balance, -itemPrice);
+    });
+
+  it('should return store items',
+    () => {
+      const storeCode = 'NCL';
+      const account = registerAccount(storeCode);
+
+      const sessionData = getSessionData(account.id);
+
+      const expectedItems = getItems(storeCode);
+      assert.deepEqual(sessionData.store.items, expectedItems);
+    });
+
+  it('should return user\'s default Store Code',
+    () => {
+      const storeCode = 'NCL';
+      // When registering, we set the user's current store to be 'storeCode'
+      const account = registerAccount(storeCode);
+
+      const sessionData = getSessionData(account.id);
+
+      assert.equal(sessionData.store.code, storeCode);
+    });
+});
