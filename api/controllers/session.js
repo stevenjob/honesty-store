dcc4108be00ed4de147d31d0a1159099ab4224ce
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-04 15:35:36 +0000

    Update session controller to use session service

--- api/controllers/session.js
+++ api/controllers/session.js
@@ -1,47 +1,17 @@
 const HTTPStatus = require('http-status');
 const { authenticateRefreshToken } = require('../middleware/authenticate');
-const { getCardNumber, getDefaultStoreCode, getAccessToken } = require('../services/accounts');
-const { getTransactionHistory, getBalance } = require('../services/transactions');
-const { getItems } = require('../services/store');
-
-const maskCardNumber = (cardNumber) => {
-  if (cardNumber != null) {
-    return cardNumber.replace(/\d(?=\d{4})/g, 'X');
-  }
-  return null;
-};
-
-const getUserSessionData = (userID) => {
-  const cardNumber = getCardNumber(userID);
-  const maskedCardNumber = maskCardNumber(cardNumber);
-  return {
-    balance: getBalance(userID),
-    transactions: getTransactionHistory(userID),
-    cardNumber: maskedCardNumber,
-  };
-};
-
-const getStoreSessionData = (userID) => {
-  const connectedStoreCode = getDefaultStoreCode(userID);
-  return {
-    items: getItems(connectedStoreCode),
-    code: connectedStoreCode,
-  };
-};
+const { getAccessToken } = require('../services/accounts');
+const getSessionData = require('../services/session');
 
 const setupSessionEndpoint = (router) => {
   router.post(
     '/session',
     authenticateRefreshToken,
     (request, response) => {
+      const sessionResponse = getSessionData(request.accountID);
+      sessionResponse.accessToken = getAccessToken(request.accountID);
       response.status(HTTPStatus.OK)
-        .json({
-          response: {
-            user: getUserSessionData(request.accountID),
-            store: getStoreSessionData(request.accountID),
-            accessToken: getAccessToken(request.accountID),
-          },
-        });
+        .json({ response: sessionResponse });
     });
 };
 
