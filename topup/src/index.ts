fc8a6d729fa9fbbcca9f7f756f5102ac18771edb
Robert Pilling <rpilling@scottlogic.co.uk>
2017-01-05 08:24:53 +0000

    respond to load balancer probes

--- topup/src/index.ts
+++ topup/src/index.ts
@@ -2,6 +2,11 @@ import express = require('express');
 
 const app = express();
 
+// send healthy response to load balancer probes
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
 app.get('/topup', (req, res) => {
   res.sendStatus(200);
 });
