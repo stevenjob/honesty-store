c7f217aaad23c9d16f9af96a754ae66c46e58312
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-06 12:43:59 +0000

    Add controller to express app

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const registerController = require('./controllers/register');
 const sessionController = require('./controllers/session');
 const signInController = require('./controllers/signin');
+const topUpController = require('./controllers/topup');
 
 const app = express();
 const router = express.Router();
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 registerController(router);
 sessionController(router);
 signInController(router);
+topUpController(router);
 
 app.use('/api/v1', router);
 
