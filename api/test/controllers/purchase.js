36d2ef80a972b7b9a56332850eb25e4d11482555
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-06 17:53:44 +0000

    Add basic tests for purchasing items
    These tests are from thorough, but they're reasonable enough for now
    until we pop the real services in.

---
+++ api/test/controllers/purchase.js
@@ -0,0 +1,70 @@
+const request = require('request');
+const assert = require('chai').assert;
+const HTTPStatus = require('http-status');
+
+const { registerAccount, __accounts } = require('../../src/services/accounts');
+const { getTransactionHistory, getBalance } = require('../../src/services/transactions');
+const { getPrice } = require('../../src/services/store');
+
+require('../../src/app');
+
+const baseURL = 'http://localhost:3000/api/v1';
+
+describe('/purchase', () => {
+  beforeEach(() => {
+    __accounts.length = 0;
+  });
+
+  it('should return \'OK\' response and error message when given invalid Item ID',
+    (done) => {
+      const { accessToken } = registerAccount('NCL');
+
+      request.post({
+        uri: `${baseURL}/purchase`,
+        auth: {
+          bearer: accessToken,
+        },
+        json: true,
+        body: {
+          itemID: 100,
+        },
+      },
+      (error, response, body) => {
+        assert.equal(response.statusCode, HTTPStatus.OK);
+        assert.property(body, 'error');
+        done();
+      });
+    });
+
+  it('should return \'OK\' response and response data when given valid Item ID',
+    (done) => {
+      const itemID = 1;
+      const { id, accessToken } = registerAccount('NCL');
+
+      const balanceBeforePurchase = getBalance(id);
+
+      request.post({
+        uri: `${baseURL}/purchase`,
+        auth: {
+          bearer: accessToken,
+        },
+        json: true,
+        body: {
+          itemID,
+        },
+      },
+      (error, response, body) => {
+        assert.equal(response.statusCode, HTTPStatus.OK);
+
+        const itemPrice = getPrice(itemID);
+        const expectedBalance = balanceBeforePurchase - itemPrice;
+        assert.equal(body.response.balance, expectedBalance);
+
+        const transactions = getTransactionHistory(id);
+        const lastTransaction = transactions[0];
+
+        assert.deepEqual(body.response.transaction, lastTransaction);
+        done();
+      });
+    });
+});
