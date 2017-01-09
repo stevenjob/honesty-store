cd68d606a4b272e46953f381e9be94b92cc67008
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-09 14:13:14 +0000

    Add transaction controller

---
+++ api/src/controllers/transactions.js
@@ -0,0 +1,36 @@
+const HTTPStatus = require('http-status');
+const { authenticateAccessToken } = require('../middleware/authenticate');
+const { getTransactionHistory } = require('../services/transactions');
+
+const getPagedTransactions = (userID, page) => {
+  const allUserTransactions = getTransactionHistory(userID);
+  const transactionsPerPage = 10;
+
+  const lastPage = Math.max(
+    Math.ceil(allUserTransactions.length / transactionsPerPage) - 1,
+    0);
+
+  const startIndex = transactionsPerPage * page;
+  const endIndex = Math.min(startIndex + transactionsPerPage, allUserTransactions.length);
+  const items = allUserTransactions.slice(startIndex, endIndex);
+
+  return { items, lastPage };
+};
+
+const setupTransactionsEndpoint = (router) => {
+  router.get(
+    '/transactions',
+    authenticateAccessToken,
+    (request, response) => {
+      const page = parseInt(request.query.page, 10) || 0;
+
+      const responseData = {
+        response: getPagedTransactions(request.accountID, page),
+      };
+
+      response.status(HTTPStatus.OK)
+        .json(responseData);
+    });
+};
+
+module.exports = setupTransactionsEndpoint;
