15eceade1f87cdabfc0a75823ec2498b25741930
Chris Price <price.c@gmail.com>
2017-01-12 15:13:10 +0000

    display only chrome

--- web/src/index.js
+++ web/src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
-import App from './app';
-import BuyScreen from './buy-screen/buy-screen';
-import StoreScreen from './store-screen/store-screen';
-import SuccessfulPurchaseScreen from './successful-purchase-screen/successful-purchase-screen';
-import './index.css';
+import Root from './chrome/root';
+
 
 ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/:storeName" component={StoreScreen}/>
-      <Route path="/:storeName/buy/:productId" component={BuyScreen}/>
-      <Route path="/:storeName/success" component={SuccessfulPurchaseScreen}/>
-    </Route>
-  </Router>
+  <Root/>
 ), document.getElementById('root'));
