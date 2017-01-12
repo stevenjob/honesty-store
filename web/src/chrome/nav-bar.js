e30458aa3e8c53d8f46300225d6b4a3c36da36eb
Chris Price <price.c@gmail.com>
2017-01-12 18:22:25 +0000

    add active styling to nav-bar

--- web/src/chrome/nav-bar.js
+++ web/src/chrome/nav-bar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router'
-import { BRAND_WHITE } from './colors';
+import { BRAND_DARK, BRAND_WHITE, MUTED_TEXT } from './colors';
 import './nav-bar.css'
 
 export default ({ storeId }) => (
   <nav className="chrome-nav-bar" style={{ background: BRAND_WHITE }}>
     <div style={{ background: BRAND_WHITE }}>
-      <Link to={`/${storeId}/store`}>Store</Link>
-      <Link to={`/${storeId}/history`}>History</Link>
-      <Link to={`/${storeId}/profile`}>Profile</Link>
-      <Link to={`/${storeId}/help`}>Help</Link>
+      <Link to={`/${storeId}/store`} style={{ color: MUTED_TEXT }} activeStyle={{ color: BRAND_DARK }}>Store</Link>
+      <Link to={`/${storeId}/history`} style={{ color: MUTED_TEXT }} activeStyle={{ color: BRAND_DARK }}>History</Link>
+      <Link to={`/${storeId}/profile`} style={{ color: MUTED_TEXT }} activeStyle={{ color: BRAND_DARK }}>Profile</Link>
+      <Link to={`/${storeId}/help`} style={{ color: MUTED_TEXT }} activeStyle={{ color: BRAND_DARK }}>Help</Link>
     </div>
   </nav>
 );
