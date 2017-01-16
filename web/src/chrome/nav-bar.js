efc7dcabbceb8cb708cfe2dca86ade9927a3812b
Chris Price <price.c@gmail.com>
2017-01-16 15:07:27 +0000

    add nav-bar icons

--- web/src/chrome/nav-bar.js
+++ web/src/chrome/nav-bar.js
@@ -3,21 +3,21 @@ import { Link } from 'react-router';
 import { BRAND_DARK, BRAND_WHITE, MUTED_TEXT } from './colors';
 import './nav-bar.css';
 
-const NavLink = ({ path, label, storeId }) => (
+const NavLink = ({ path, icon, label, storeId }) => (
   <Link to={`/${storeId}${path}`}
     style={{ color: MUTED_TEXT }}
-    activeStyle={{ color: BRAND_DARK }}>
+    activeStyle={{ color: BRAND_DARK }}
+    activeClassName="chrome-nav-bar-active">
+    <i className={`chrome-nav-bar-icon-${icon}`}></i>
     {label}
   </Link>
 );
 
 export default ({ storeId }) => (
   <nav className="chrome-nav-bar" style={{ background: BRAND_WHITE }}>
-    <div style={{ background: BRAND_WHITE }}>
-      <NavLink storeId={storeId} path="/store" label="Store"/>
-      <NavLink storeId={storeId} path="/history" label="History"/>
-      <NavLink storeId={storeId} path="/profile" label="Profile"/>
-      <NavLink storeId={storeId} path="/help" label="Help"/>
-    </div>
+    <NavLink storeId={storeId} path="/store" icon="store" label="Store"/>
+    <NavLink storeId={storeId} path="/history" icon="history" label="History"/>
+    <NavLink storeId={storeId} path="/profile" icon="profile" label="Profile"/>
+    <NavLink storeId={storeId} path="/help" icon="help" label="Help"/>
   </nav>
 );
