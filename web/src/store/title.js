f23024821946b01761288ff14b762bcf13c9fb2b
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-16 14:49:51 +0000

    Connect state balance to StoreTitle Balance component

--- web/src/store/title.js
+++ web/src/store/title.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import TitleBar from '../chrome/title-bar';
 import './title.css';
 
-const Balance = () => <div className="store-title-balance">
-  <small>Balance</small>
-  <h1><small>£</small>6.45</h1>
-</div>;
+const Balance = ({ balance }) => {
+  const formattedBalance = (balance/100).toFixed(2);
+  return (
+    <div className="store-title-balance">
+      <small>Balance</small>
+      <h1><small>£</small>{formattedBalance}</h1>
+    </div>
+  );
+};
 
-export default ({ params: { storeId } }) => (
-  <TitleBar title="Store" subtitle={`@${storeId}`} right={<Balance />} />
+const StoreTitle = (props) => (
+  <TitleBar
+    title="Store" 
+    subtitle={`@${props.params.storeId}`} 
+    right={<Balance balance={props.balance}/>} 
+  />
 );
 
+const mapStateToProps = (state) => {
+  const balance = state.user.balance || 0;
+  return { balance };
+};
 
+export default connect(mapStateToProps)(StoreTitle);
