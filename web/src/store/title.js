3ea3939b21915cdc43c5cd2e0e574153930e1c3b
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-16 12:26:19 +0000

    Add balance to store-title

--- web/src/store/title.js
+++ web/src/store/title.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import TitleBar from '../chrome/title-bar';
+import './title.css';
+
+const Balance = () => <div className="store-title-balance">
+  <small>Balance</small>
+  <h1><small>£</small>6.45</h1>
+</div>;
 
 export default ({ params: { storeId } }) => (
-  <TitleBar title="Store" subtitle={`@${storeId}`}/>
+  <TitleBar title="Store" subtitle={`@${storeId}`} right={<Balance />} />
 );
 
 
