9b84b456310f07dee58ca89d38c074b58f61ad33
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-17 15:13:50 +0000

    Set styles on chrome-list

--- web/src/chrome/list.js
+++ web/src/chrome/list.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { MUTED_TEXT } from './colors';
+import { MUTED_TEXT, LIGHT_BACKGROUND } from './colors';
 import './list.css';
 
 export default ({ data, itemRenderer }) => (
-  <ul className="chrome-list">
+  <ul 
+    className="chrome-list"
+    style={{borderBottomColor: MUTED_TEXT, backgroundColor: LIGHT_BACKGROUND}}
+  >
     {
       data == null ? <p>'No Data'</p> :
       data.map((item, index) => (
