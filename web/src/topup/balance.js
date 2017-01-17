4b55bf39ef099adeeac6f97f489e4c3d2df0070f
Chris Price <price.c@gmail.com>
2017-01-17 11:20:02 +0000

    add styling to balance page

--- web/src/topup/balance.js
+++ web/src/topup/balance.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { BRAND_LIGHT } from '../chrome/colors';
+import { BRAND_LIGHT, MUTED_TEXT } from '../chrome/colors';
 import Button from '../chrome/button';
 import Page from '../chrome/page';
 import './balance.css';
@@ -10,17 +10,21 @@ export default ({ params: { storeId } }) =>
         title="Balance"
         storeId={storeId}
         invert={true}
-        nav={false}>
+        nav={false}
+        fullscreen={true}>
         <div className="topup-balance">
             <div className="topup-balance-balance">
                 <h3>Your balance is currently</h3>
-                <h1><small>£</small>6.45</h1>
+                <h1 style={{color: BRAND_LIGHT}}><small>£</small>6.45</h1>
             </div>
             <div className="topup-balance-topup">
                 <h2>Would you like to top up?</h2>
-                <h1><small>£</small>5</h1>
-                <p>Your balance will be</p>
-                <p>£11.45</p>
+                <div className="topup-balance-topup-amount">
+                    <Button>-</Button>
+                    <h1 style={{color: BRAND_LIGHT}}><small>£</small>5</h1>
+                    <Button>+</Button>
+                </div>
+                <p style={{color: MUTED_TEXT}}>Your balance will be<br/>£11.45</p>
                 <p><Button to={`/${storeId}/topup/500`}>Top Up using a Card</Button></p>
             </div>
         </div>
