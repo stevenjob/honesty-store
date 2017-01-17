ff227606b64f6a2b04d54420400cc7abd17d2791
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-17 15:01:20 +0000

    Update mock API to use 'real' data

--- api/src/services/store.js
+++ api/src/services/store.js
@@ -1,10 +1,17 @@
 const items = new Map();
-items.set(0, { name: 'Twix', price: 20 });
-items.set(1, { name: 'Mars', price: 30 });
+items.set(0, { name: 'Walkers Cheese & Onion', price: 50 });
+items.set(1, { name: 'Nature Valley Crunchy Oats & Honey', price: 40 });
+items.set(2, { name: 'Snickers', price: 51 });
+items.set(3, { name: 'Skittles', price: 48 });
+items.set(4, { name: 'KitKat Chunky', price: 58 });
+
+const allItems = [0, 1, 2, 3, 4];
 
 const stores = new Map();
-stores.set('SL-NCL', [0, 1]);
-stores.set('SL-EDN', [1]);
+stores.set('sl-ncl', allItems);
+stores.set('sl-edn', allItems);
+stores.set('sl-brs', allItems);
+stores.set('sl-ldn', allItems);
 
 const getPrice = (itemID) => {
   const item = items.get(itemID);
