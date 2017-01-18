2815a4ead93f8981f295ffae9f19c13f1dccd0bf
Robert Pilling <rpilling@scottlogic.co.uk>
2017-01-18 17:54:03 +0000

    simplify session endpoint

--- api/src/controllers/session.ts
+++ api/src/controllers/session.ts
@@ -3,17 +3,13 @@ import { authenticateRefreshToken } from '../middleware/authenticate';
 import { getSessionData, SessionData } from '../services/session';
 import { promiseResponse } from '../../../service/src/endpoint-then-catch';
 
-const updateSession = async (userID) => {
-  return await getSessionData(userID);
-};
-
 export default (router) => {
   router.post(
     '/session',
     authenticateRefreshToken,
     (request, response) => {
       promiseResponse<SessionData>(
-          updateSession(request.userID),
+          getSessionData(request.userID),
           response,
           HTTPStatus.INTERNAL_SERVER_ERROR);
     });
