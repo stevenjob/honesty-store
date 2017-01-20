4e518ff1f5839895fcd17d1e71307b34cd12dfc0
Robert Pilling <rpilling@scottlogic.co.uk>
2017-01-20 12:51:54 +0000

    emit instantiation backtrace from promiseResponse() on error

--- service/src/endpoint-then-catch.ts
+++ service/src/endpoint-then-catch.ts
@@ -3,13 +3,20 @@ import * as winston from 'winston';
 
 // generic to allow callers to assert the type of what's being sent back
 export const promiseResponse = <T>(promise: Promise<T>, response, httpErrorCode = HTTPStatus.OK) => {
+    const backtrace = new Error().stack;
+
     promise
         .then((result: T) => {
             response.status(HTTPStatus.OK)
                 .json({ response: result });
         })
         .catch((error) => {
-            winston.error(`promiseResponse() failed, returning HTTP ${httpErrorCode}`, error);
+            winston.error(
+                `promiseResponse() failed, returning HTTP ${httpErrorCode}`,
+                error,
+                'instantiation backtrace:',
+                backtrace);
+
             response.status(httpErrorCode)
                 .json({ error: { message: error.message }});
         });
