495bcbb54244679208c052c23bd4e8f2cec4696d
Chris Price <price.c@gmail.com>
2017-01-23 14:33:16 +0000

    add email validation and wire into register process

--- web/src/register/email.js
+++ web/src/register/email.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
 import Button from '../chrome/button';
-import { BRAND_LIGHT } from '../chrome/colors';
+import { BRAND_LIGHT, DANGER } from '../chrome/colors';
 import { NotNow } from '../chrome/link';
 import Page from '../chrome/page';
+import isEmail from 'validator/lib/isEmail';
 import './email.css';
 
 export default class extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailAddress: ''
+        };
+    }
+
+    handleChange(event) {
+        const emailAddress = event.target.value;
+        this.setState({ emailAddress });
+    }
+
+    handleSubmit() {
         const { params: { storeId, itemId } } = this.props;
-        const emailAddress = 'foo@example.com';
+        const { emailAddress } = this.state;
+        const valid = isEmail(emailAddress);
+        this.setState({ valid });
+        if (valid) {
+            hashHistory.push(`/${storeId}/register/${itemId}/${emailAddress}`);
+        }
+    }
+
+    style() {
+        const { valid } = this.state;
+        return valid === false ? { borderBottomColor: DANGER } : {};
+    }
+
+    render() {
+        const { params: { storeId } } = this.props;
+        const { valid } = this.state;
         return <Page left={<NotNow/>}
             title={`Register`}
             storeId={storeId}
@@ -18,9 +47,20 @@ export default class extends React.Component {
             fullscreen={true}>
             <div className="register-email">
                 <h2>Want to sign up to {storeId}?</h2>
-                <p>If you want to sign up for an account please enter your email address below</p>
-                <p><input type="email" placeholder="honest.jo@honesty.store"/></p>
-                <p><Button to={`/${storeId}/register/${itemId}/${emailAddress}`}>Continue to Top Up</Button></p>
+                {
+                    valid !== false ?
+                    <p>If you want to sign up for an account please enter your email address below</p>
+                    :
+                    <p style={{ color: DANGER }}>Please enter a valid email address</p>
+                }
+                <p>
+                    <input type="email"
+                        name="emailAddress"
+                        placeholder="honest.jo@honesty.store"
+                        onChange={(e) => this.handleChange(e)}
+                        style={this.style()}/>
+                </p>
+                <p><Button onClick={() => this.handleSubmit()}>Continue to Top Up</Button></p>
                 <p><Link to={`/${storeId}/signin`} style={{ color: BRAND_LIGHT }}>Already have an account?</Link></p>
             </div>
         </Page>;
