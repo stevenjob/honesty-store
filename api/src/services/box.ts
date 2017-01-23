5094a1fe4ea6dd925997415e0fe323a40bf9c199
Chris Price <price.c@gmail.com>
2017-01-23 19:03:38 +0000

    fix item id references from box

--- api/src/services/box.ts
+++ api/src/services/box.ts
@@ -1,11 +1,11 @@
 const boxes = new Map();
 boxes.set(0, {
   items: [
-    { itemID: '0', count: 10 },
-    { itemID: '1', count: 35 },
-    { itemID: '2', count: 28 },
-    { itemID: '3', count: 20 },
-    { itemID: '4', count: 27 },
+    { itemID: '46ced0c0-8815-4ed2-bfb6-40537f5bd512', count: 10 },
+    { itemID: 'faeda516-bd9f-41ec-b949-7a676312b0ae', count: 35 },
+    { itemID: 'b43c4a97-1112-41ce-8f91-5a8bda0dcdc8', count: 28 },
+    { itemID: '78816fba-150d-4282-b43d-900df45cea8b', count: 20 },
+    { itemID: '3fa0db7c-3f90-404e-b875-3792eda3e185', count: 27 },
   ],
 });
 
