c09e208f2ac1c773f42415afbc794bddc04574ff
Sam Burnstone <sburnstone@scottlogic.com>
2017-01-23 15:31:27 +0000

    Add history list items to history page

--- web/src/history/index.js
+++ web/src/history/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import List from '../chrome/list';
+// import List from '../chrome/list';
 import Page from '../chrome/page';
+import HistoryItem from './item';
 import './index.css';
 
-const itemRenderer = (data, index) => (
-  <b>{JSON.stringify(data)}</b>
-);
-
-const History = ({ transactions, params: { storeId }, loading }) => (
+const History = ({ transactions = [], params: { storeId }, loading }) => (
   <Page title="History"
     storeId={storeId}
     loading={loading}>
-    <List data={transactions} itemRenderer={itemRenderer}/>
+    <ul className="history-list">
+      { [
+        { type: 'topup', amount: 500 }, 
+        { type: 'purchase', amount: 50, data: { itemId: 0, quantity: 1 }}
+      ].map((transaction, index) => <li key={index}><HistoryItem transaction={transaction}/></li>) }
+    </ul>
   </Page>
 );
 
