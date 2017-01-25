e551dee709861cafbf5eaef6bcaefb76e8067911
Robert Pilling <rpilling@scottlogic.co.uk>
2017-01-25 15:41:30 +0000

    factor out common code in router - get/post/put

--- service/src/router.ts
+++ service/src/router.ts
@@ -9,17 +9,13 @@ interface Params {
     [key: string]: string;
 }
 
-interface Action<Result> {
-    (key: Key, params: Params): Promise<Result>;
-}
-
 interface BodyAction<Result, Body> {
     (key: Key, params: Params, body: Body): Promise<Result>;
 }
 
 interface Router {
     (request, response, next): void;
-    get<Result>(path: string, version: number, action: Action<Result>);
+    get<Result>(path: string, version: number, action: BodyAction<void, Result>);
     post<Body, Result>(path: string, version: number, action: BodyAction<Body, Result>);
     put<Body, Result>(path: string, version: number, action: BodyAction<Body, Result>);
 }
@@ -41,77 +37,37 @@ const serviceSecretInvalid = (request, response): boolean => {
     }
 };
 
+const createEndPoint = (service, internalRouter, method) => <Body, Result>(path, version: number, action: BodyAction<Body, Result>) => {
+    internalRouter[method/* .get/.post/.put */](`/${service}/v${version}${path}`, (request, response) => {
+        if (serviceSecretInvalid(request, response)) {
+            return;
+        }
+
+        const key = extractKey(request);
+        info(key, `handling ${method} ${request.url}`);
+        action(key, request.params, /* maybe undefined */request.body)
+            .then(result => {
+                info(key, `successful ${method} ${request.url}`, result);
+                response.status(HTTPStatus.OK)
+                    .json({ response: result });
+            })
+            .catch((e) => {
+                error(key, `failed ${method} ${request.url}`, e);
+                response.status(200)
+                    .json({ error: { message: e.message }});
+            });
+    });
+};
+
 export default (service: string): Router => {
     const internalRouter = express.Router();
 
     const router: any = (request, response, next) =>
         internalRouter(request, response, next);
 
-    router.get = <Result>(path, version: number, action: Action<Result>) => {
-        internalRouter.get(`/${service}/v${version}${path}`, (request, response) => {
-            if (serviceSecretInvalid(request, response)) {
-                return;
-            }
-
-            const key = extractKey(request);
-            info(key, `handling GET ${request.url}`);
-            action(key, request.params)
-                .then(result => {
-                    info(key, `successful GET ${request.url}`, result);
-                    response.status(HTTPStatus.OK)
-                        .json({ response: result });
-                })
-                .catch((e) => {
-                    error(key, `failed GET ${request.url}`, e);
-                    response.status(200)
-                        .json({ error: { message: e.message }});
-                });
-        });
-    };
-
-    router.post = <Body, Result>(path, version: number, action: BodyAction<Body, Result>) => {
-        internalRouter.post(`/${service}/v${version}${path}`, (request, response) => {
-            if (serviceSecretInvalid(request, response)) {
-                return;
-            }
-
-            const key = extractKey(request);
-            info(key, `handling POST ${request.url}`, request.body);
-            action(key, request.params, request.body)
-                .then(result => {
-                    info(key, `successful POST ${request.url}`, result);
-                    response.status(HTTPStatus.OK)
-                        .json({ response: result });
-                })
-                .catch((e) => {
-                    error(key, `failed POST ${request.url}`, e);
-                    response.status(200)
-                        .json({ error: { message: e.message }});
-                });
-        });
-    };
-
-    router.put = <Body, Result>(path, version: number, action: BodyAction<Body, Result>) => {
-        internalRouter.put(`/${service}/v${version}${path}`, (request, response) => {
-            if (serviceSecretInvalid(request, response)) {
-                return;
-            }
-
-            const key = extractKey(request);
-            info(key, `handling PUT ${request.url}`, request.body);
-            action(key, request.params, request.body)
-                .then(result => {
-                    info(key, `successful PUT ${request.url}`, result);
-                    response.status(HTTPStatus.OK)
-                        .json({ response: result });
-                })
-                .catch((e) => {
-                    error(key, `failed PUT ${request.url}`, e);
-                    response.status(200)
-                        .json({ error: { message: e.message }});
-                });
-        });
-    };
+    router.get = createEndPoint(service, internalRouter, 'get');
+    router.post = createEndPoint(service, internalRouter, 'post');
+    router.put = createEndPoint(service, internalRouter, 'put');
 
     return router;
 };
