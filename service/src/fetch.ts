20728f6c4560b7ea61c28104e8b255b7b860d8a0
Robert Pilling <rpilling@scottlogic.co.uk>
2017-01-25 15:05:41 +0000

    factor out get, post and put methods

--- service/src/fetch.ts
+++ service/src/fetch.ts
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import { baseUrl } from './baseUrl';
 import { Key } from './key';
-import { info } from './log';
+import { info, error } from './log';
 
 interface ApiResponse<T> {
     response?: T;
@@ -9,56 +9,67 @@ interface ApiResponse<T> {
 }
 
 export default (service: string) => {
-
-    const get = async <Result>(version: number, key: Key, path: String) => {
+    const fetchAndParse = async <Result>({ method, version, path, key, headers = {}, body = undefined }): Promise<Result> => {
         const url = `${baseUrl}/${service}/v${version}${path}`;
-        info(key, `send GET ${url}`);
+
+        info(key, `send ${method} ${url}`);
+
         const response = await fetch(url, {
+            method,
             headers: {
                 key: JSON.stringify(key),
-            }
-        })
-            .then<ApiResponse<Result>>(response => response.json());
-        if (response.error) {
-            throw new Error(response.error.message);
+                ...headers,
+            },
+            body: body ? JSON.stringify(body) : undefined,
+        });
+
+        let json: ApiResponse<Result>;
+        try {
+            json = await response.json();
+        } catch (e) {
+            throw new Error(`${method} ${path} failed (couldn't parse json), HTTP ${response.status}`);
         }
-        return response.response;
+
+        if (json.error) {
+            throw new Error(json.error.message);
+        }
+
+        return json.response;
+    };
+
+    const get = async <Result>(version: number, key: Key, path: String) => {
+        return await fetchAndParse<Result>({
+            method: 'GET',
+            version,
+            path,
+            key,
+        });
     };
 
     const post = async <Result>(version: number, key: Key, path: String, body: any): Promise<Result> => {
-        const url = `${baseUrl}/${service}/v${version}${path}`;
-        info(key, `send POST ${url}`);
-        const response = await fetch(url, {
+        return await fetchAndParse<Result>({
             method: 'POST',
+            version,
+            path,
             headers: {
                 'content-type': 'application/json',
-                key: JSON.stringify(key),
             },
-            body: JSON.stringify(body)
-        })
-            .then<ApiResponse<Result>>(response => response.json());
-        if (response.error) {
-            throw new Error(response.error.message);
-        }
-        return response.response;
+            key,
+            body,
+        });
     };
 
     const put = async <Result>(version: number, key: Key, path: String, body: any): Promise<Result> => {
-        const url = `${baseUrl}/${service}/v${version}${path}`;
-        info(key, `send PUT ${url}`);
-        const response = await fetch(url, {
+        return await fetchAndParse<Result>({
             method: 'PUT',
+            version,
+            path,
             headers: {
                 'content-type': 'application/json',
-                key: JSON.stringify(key),
             },
-            body: JSON.stringify(body)
-        })
-            .then<ApiResponse<Result>>(response => response.json());
-        if (response.error) {
-            throw new Error(response.error.message);
-        }
-        return response.response;
+            key,
+            body,
+        });
     };
 
     return {
