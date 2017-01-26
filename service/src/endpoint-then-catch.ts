1332b401e50ad75d2a84f11d17fa582c2d3aeb89
Robert Pilling <rpilling@scottlogic.co.uk>
2017-01-26 10:50:16 +0000

    catch previous commit's bug at compile time

--- service/src/endpoint-then-catch.ts
+++ service/src/endpoint-then-catch.ts
@@ -1,9 +1,15 @@
 import HTTPStatus = require('http-status');
+import express = require('express');
 import { Key } from './key';
 import { error } from './log';
 
 // generic to allow callers to assert the type of what's being sent back
-export const promiseResponse = <T>(promise: Promise<T>, request /*: express.Request*/, response, httpErrorCode = HTTPStatus.OK) => {
+export const promiseResponse = <T>(
+    promise: Promise<T>,
+    request: express.Request & { key: Key },
+    response: express.Response,
+    httpErrorCode = HTTPStatus.OK
+) => {
     const backtrace = new Error().stack;
 
     promise
