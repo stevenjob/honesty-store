68d835cba7879ac1cfc37d03e1108680dacb0a9d
Robert Pilling <rpilling@scottlogic.co.uk>
2017-01-30 15:40:14 +0000

    pass extra json data into custom-topup on the commandline

--- support/src/custom-topup.ts
+++ support/src/custom-topup.ts
@@ -9,21 +9,22 @@ const usage = () => {
   const script = process.argv[1];
   const [, filename = script] = script.match(/\/([^/]+)$/);
 
-  console.error(`Usage: ${filename} userId amount`);
+  console.error(`Usage: ${filename} userId amount extra-json-data`);
   console.error(`  $SERVICE_TOKEN_SECRET and $BASE_URL required in the environment`);
   console.error(``);
-  console.error(`  e.g. SERVICE_TOKEN_SECRET=service:xNxznoraYHUq+A0O33OJ23pwwfQAtK12SCt BASE_URL=https://honesty.store ${filename} 56b60280-5823-4058-9b5b-24f4c2660876 500`);
+  console.error(`  e.g. SERVICE_TOKEN_SECRET=service:xNxznoraYHUq+A0O33OJ23pwwfQAtK12SCt BASE_URL=https://honesty.store ${filename} 56b60280-5823-4058-9b5b-24f4c2660876 500  '{ "scottcoinTx": "<scottcoin-block-hash>" }'`);
   process.exit(2);
 }
 
 const main = async (args) => {
-  if (args.length !== 2) {
+  if (args.length !== 3) {
     usage();
   }
 
-  const [ userId, amountArgument ] = args;
+  const [ userId, amountArgument, jsonArgument ] = args;
 
   const amount = Number(amountArgument);
+  const json = JSON.parse(jsonArgument);
 
   const correlationKey = createUserKey({ userId });
 
@@ -35,10 +36,10 @@ const main = async (args) => {
     {
       type: 'topup',
       amount,
-      data: {
-        isSupportTopup: 'true', // must be a string
-      }
+      data: json
     });
+
+  console.log(`topup transaction: ${JSON.stringify(supportTopupTx)}`);
 };
 
 main(process.argv.slice(2))
