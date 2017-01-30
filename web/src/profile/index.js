c1410cdf0a752d700fb0abf2582f8f19214918fa
Chris Price <price.c@gmail.com>
2017-01-30 19:32:07 +0000

    remove fake profile details

--- web/src/profile/index.js
+++ web/src/profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { BRAND_LIGHT, LIGHT_TEXT, MUTED_TEXT, DANGER, LIGHT_BACKGROUND } from '../chrome/colors';
+import { MUTED_TEXT, DANGER, LIGHT_BACKGROUND } from '../chrome/colors';
 import Page from '../chrome/page';
 import './index.css';
 
@@ -9,10 +9,11 @@ const Profile = ({ emailAddress }) => (
   <Page title="Profile">
     <div>
       <div className="profile-badge" style={{ borderColor: MUTED_TEXT, background: LIGHT_BACKGROUND }}>
-        <div className="profile-badge-image" style={{ background: BRAND_LIGHT, color: LIGHT_TEXT }}>HJ</div>
         <div className="profile-badge-details">
-          <div className="profile-badge-details-name">Honest Jo</div>
-          <div className="profile-badge-details-email">{emailAddress}</div>
+          <div className="profile-badge-details-email">
+            <h3>Email</h3>
+            {emailAddress}
+          </div>
         </div>
         <div className="profile-badge-action">
           <Link to={`/profile/edit`}>Edit</Link>
