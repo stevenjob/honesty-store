b90f04708016d11246ef97290d8e4b15274de3c9
Robert Pilling <rpilling@scottlogic.co.uk>
2017-02-01 11:57:06 +0000

    factor out common createKey() code

--- service/src/key.ts
+++ service/src/key.ts
@@ -11,31 +11,46 @@ export interface AuthenticationKey extends Key {
   setUserId(userId: string);
 }
 
+// return a key like so: `service:correlationKey:tag1=value1:tag2=value2:...`
+const createKey = ({ service, correlationKey = uuid(), tags = {} }) => {
+  const tagString = Object.keys(tags).map((k) => `${k}=${tags[k]}`).join(':');
+
+  return <Key>{
+    __IS__A__KEY__: void 0,
+    toJSON() {
+      return this.toString();
+    },
+    toString() {
+      return `${service}:${correlationKey}:${tagString}`;
+    },
+    service,
+    correlationKey,
+    tags
+  };
+};
+
 export const createUserKey = ({ userId, correlationKey = undefined }) => {
   if (!isUUID(userId)) {
     throw new Error(`Invalid userId specified ${userId}`);
   }
-  return <Key>{
-    toJSON() {
-      return this.toString();
-    },
-    toString() {
-      return `user:${userId}:${correlationKey || uuid()}`;
-    }
-  };
+  return createKey({
+    service: 'user',
+    correlationKey: correlationKey || uuid(),
+    tags: { userId }
+  });
 };
 
 export const createAuthenticationKey = () => {
   const correlationKey = uuid();
+  const key = createKey({
+    service: 'auth'
+  });
+
   return <AuthenticationKey>{
+    ...key,
+
     setUserId(userId: string): Key {
       return createUserKey({ userId, correlationKey });
-    },
-    toJSON() {
-      return this.toString();
-    },
-    toString() {
-      return `auth:${correlationKey}`;
     }
   };
 };
@@ -44,36 +59,16 @@ export const createEmailKey = ({ emailAddress }) => {
   if (!isEmail(emailAddress)) {
     throw new Error(`Invalid emailAddress specified ${emailAddress}`);
   }
-  const correlationKey = uuid();
-  return <Key>{
-    toJSON() {
-      return this.toString();
-    },
-    toString() {
-      return `email:${emailAddress}:${correlationKey}`;
-    }
-  };
+  return createKey({
+    service: 'email',
+    tags: { emailAddress }
+  });
 };
 
 export const createUnauthenticatedKey = () => {
-  const correlationKey = uuid();
-  return <Key>{
-    toJSON() {
-      return this.toString();
-    },
-    toString() {
-      return `unauthenticated:${correlationKey}`;
-    }
-  };
+  return createKey({ service: 'unauthenticated' });
 };
 
 export const createServiceKey = ({ name }) => {
-  return <Key>{
-    toJSON() {
-      return this.toString();
-    },
-    toString() {
-      return `service:${name}`;
-    }
-  };
+  return createKey({ service: name });
 };
