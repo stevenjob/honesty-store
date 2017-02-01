fec5233db31b6ba1c5812c4bad77f2066c00254e
Sam Burnstone <sburnstone@scottlogic.com>
2017-02-01 11:21:22 +0000

    Lint 'test' directory

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,3 +1,4 @@
+// tslint:disable no-console
 import fetch from 'node-fetch';
 import Stripe = require('stripe');
 
@@ -9,21 +10,21 @@ if (process.argv.length !== 3) {
 const baseUrl = process.argv[2];
 const version = 1;
 const emailAddress = `${process.env.USER}@scottlogic.co.uk`;
-const item1 = "46ced0c0-8815-4ed2-bfb6-40537f5bd512";
-const item2 = "faeda516-bd9f-41ec-b949-7a676312b0ae";
+const item1 = '46ced0c0-8815-4ed2-bfb6-40537f5bd512';
+const item2 = 'faeda516-bd9f-41ec-b949-7a676312b0ae';
 const card = {
-    "number": "4242424242424242",
-    "exp_month": 12,
-    "exp_year": 2019,
-    "cvc": "123"
+    number: '4242424242424242',
+    exp_month: 12,
+    exp_year: 2019,
+    cvc: '123'
 };
-const storeCode = "sl-ncl";
+const storeCode = 'sl-ncl';
 
 const globals = {
     refreshToken: '',
     accessToken: '',
     accountId: '',
-    userId: '',
+    userId: ''
 };
 
 const fetch2 = async (service, path, headers = undefined, body = undefined, method = 'POST') => {
@@ -34,10 +35,10 @@ const fetch2 = async (service, path, headers = undefined, body = undefined, meth
         method,
         headers: {
             ...headers,
-            'content-type': 'application/json',
+            'content-type': 'application/json'
         },
-        body: body ? JSON.stringify(body) : undefined,
-    }).then(response => response.json());
+        body: body ? JSON.stringify(body) : undefined
+    }).then(r => r.json());
 
     if (response.error) {
         throw new Error(response.error.message);
@@ -47,7 +48,7 @@ const fetch2 = async (service, path, headers = undefined, body = undefined, meth
 };
 
 const register = async () => {
-    const { user, store, accessToken, refreshToken } = await fetch2(
+    const { accessToken, refreshToken } = await fetch2(
         'api',
         '/register',
         undefined,
@@ -61,7 +62,7 @@ const generateStripeToken = async () => {
     const key = process.env.STRIPE_SECRET_KEY_TEST;
 
     if (!key) {
-        console.error("need $STRIPE_SECRET_KEY_TEST");
+        console.error('need $STRIPE_SECRET_KEY_TEST');
         process.exit(1);
     }
 
@@ -72,17 +73,17 @@ const generateStripeToken = async () => {
 };
 
 const topupAndPurchase = async () => {
-    const { user, store } = await fetch2(
+    const { user } = await fetch2(
         'api',
         '/register2',
         {
-            authorization: `Bearer: ${globals.accessToken}`,
+            authorization: `Bearer: ${globals.accessToken}`
         },
         {
             emailAddress,
             itemID: item1,
             stripeToken: await generateStripeToken(),
-            topUpAmount: 500,
+            topUpAmount: 500
         });
 
     const { accountId, id: userId } = user;
@@ -91,20 +92,20 @@ const topupAndPurchase = async () => {
 };
 
 const getSession = async () => {
-    const { user, store } = await fetch2(
+    await fetch2(
         'api',
         '/session',
         {
-            authorization: `Bearer: ${globals.refreshToken}`,
+            authorization: `Bearer: ${globals.refreshToken}`
         });
 };
 
 const topup = async () => {
-    const { balance, transaction } = await fetch2(
+    await fetch2(
         'api',
         '/topup',
         {
-            authorization: `Bearer: ${globals.accessToken}`,
+            authorization: `Bearer: ${globals.accessToken}`
         },
         {
             amount: 500
@@ -112,15 +113,15 @@ const topup = async () => {
 };
 
 const purchaseMultiple = async () => {
-    const { balance, transaction } = await fetch2(
+    await fetch2(
         'api',
         '/purchase',
         {
-            authorization: `Bearer: ${globals.accessToken}`,
+            authorization: `Bearer: ${globals.accessToken}`
         },
         {
             itemID: item2,
-            quantity: 3,
+            quantity: 3
         });
 };
 
@@ -133,5 +134,5 @@ const test = async () => {
 };
 
 test()
-    .then(() => console.log(`tests passed`))
+    .then(() => console.log('tests passed'))
     .catch((e) => { console.error(e); process.exit(1); });
