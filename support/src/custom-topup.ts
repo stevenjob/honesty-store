8beade067b71b4aed8ea2798afeccb2f10f27cfa
Sam Burnstone <sburnstone@scottlogic.com>
2017-02-03 12:51:40 +0000

    Add tslint support to support service

--- support/src/custom-topup.ts
+++ support/src/custom-topup.ts
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-import jwt = require('jsonwebtoken');
+import { createUserKey } from '../../service/src/key';
 import { createTransaction } from '../../transaction/src/client';
 import { getUser } from '../../user/src/client';
-import { createUserKey } from '../../service/src/key';
 
 const usage = () => {
   const script = process.argv[1];
@@ -13,9 +12,10 @@ const usage = () => {
   console.error(`  $SERVICE_TOKEN_SECRET and $BASE_URL required in the environment`);
   console.error(`  $BASE_URL should be https://live.honesty.store for real topups, avoiding the https://honesty.store CDN`);
   console.error(``);
+  // tslint:disable-next-line:max-line-length
   console.error(`  e.g. SERVICE_TOKEN_SECRET=service:xNxznoraYHUq+A0O33OJ23pwwfQAtK12SCt BASE_URL=https://honesty.store ${filename} 56b60280-5823-4058-9b5b-24f4c2660876 500  '{ "scottcoinTx": "<scottcoin-block-hash>" }'`);
   process.exit(2);
-}
+};
 
 const main = async (args) => {
   if (args.length !== 3) {
@@ -39,7 +39,7 @@ const main = async (args) => {
       amount,
       data: json
     });
-
+  // tslint:disable-next-line:no-console
   console.log(`topup transaction: ${JSON.stringify(supportTopupTx)}`);
 };
 
