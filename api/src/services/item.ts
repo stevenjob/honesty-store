8fb95cf1965b662a969015af65859a3e66b6b6d7
Robert Pilling <rpilling@scottlogic.co.uk>
2017-02-07 10:29:49 +0000

    add brs/edn/ldn items with new prices

--- api/src/services/item.ts
+++ api/src/services/item.ts
@@ -36,6 +36,12 @@ items.set('b43c4a97-1112-41ce-8f91-5a8bda0dcdc8', snickers(51));
 items.set('78816fba-150d-4282-b43d-900df45cea8b', skittles(48));
 items.set('3fa0db7c-3f90-404e-b875-3792eda3e185', kitKatChunky(58));
 
+items.set('28b0a802-bef3-478b-81d0-034e3ac02092', walkers(55));
+items.set('b3cad9b1-56fb-446d-987a-da79b8f75140', natureValley(45));
+items.set('9d75c0d8-0c37-4828-90a4-d8cdc0ba9582', snickers(56));
+items.set('41ed2c85-6ef6-4079-b8a3-aeeb41ba13b3', skittles(53));
+items.set('678ada89-7050-45ee-bed9-5c46da6bc053', kitKatChunky(65));
+
 export const getItem = (itemID) => {
   const item = items.get(itemID);
   if (item == null) {
