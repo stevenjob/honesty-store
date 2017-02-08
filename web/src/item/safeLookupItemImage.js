7c79dd1f82e716ddb85604b745a625680b7889e5
Chris Price <price.c@gmail.com>
2017-02-08 14:27:59 +0000

    fix naming of freddo bar

--- web/src/item/safeLookupItemImage.js
+++ web/src/item/safeLookupItemImage.js
@@ -3,6 +3,6 @@ export default (path) => {
     return require(`../item/assets/${path}`);
   } catch (e) {
     // If we can't load the image, then we'll just show a generic one
-    return require(`../item/assets/not-found.svg`);
+    return require(`../item/assets/misc-bar.svg`);
   }
 };
