90697178a68c8b574f810e3e7c764c0ad9f0f89e
Chris Price <price.c@gmail.com>
2017-02-14 09:47:48 +0000

    updated item images

--- web/src/item/misc-bar.js
+++ web/src/item/misc-bar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default () =>
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 -47 140 140">
-    <rect x="10" y="3" fill="#43BDD7" width="120" height="40" />
-    <rect x="10" y="23" fill="#39A1B7" width="120" height="20" />
-    <polygon fill="#43BDD7" points="10,43 0,46 0,0 10,3 " />
-    <polygon fill="#39A1B7" points="10,43 0,46 0,23 10,23 " />
-    <polygon fill="#43BDD7" points="130,43 140,46 140,0 130,3 " />
-    <polygon fill="#39A1B7" points="130,43 140,46 140,23 130,23 " />
-    <rect x="10" y="3" fill="#FFCD00" width="10" height="40" />
-    <rect x="10" y="23" fill="#EDAB07" width="10" height="20" />
-    <rect x="120" y="3" fill="#FFCD00" width="10" height="40" />
-    <rect x="120" y="23" fill="#EDAB07" width="10" height="20" />
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 140">
+    <rect x="10" y="50" fill="#43BDD7" width="120" height="40"/>
+    <rect x="10" y="70" fill="#39A1B7" width="120" height="20"/>
+    <polygon fill="#43BDD7" points="10,90 0,93 0,47 10,50 "/>
+    <polygon fill="#39A1B7" points="10,90 0,93 0,70 10,70 "/>
+    <polygon fill="#43BDD7" points="130,90 140,93 140,47 130,50 "/>
+    <polygon fill="#39A1B7" points="130,90 140,93 140,70 130,70 "/>
+    <rect x="10" y="50" fill="#FFCD00" width="10" height="40"/>
+    <rect x="10" y="70" fill="#EDAB07" width="10" height="20"/>
+    <rect x="120" y="50" fill="#FFCD00" width="10" height="40"/>
+    <rect x="120" y="70" fill="#EDAB07" width="10" height="20"/>
   </svg>;
