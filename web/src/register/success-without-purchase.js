d0291f22fec17a876e5699ff09cf0306478cfecb
Robert Pilling <rpilling@scottlogic.co.uk>
2017-02-15 17:31:43 +0000

    minor change to return of mapStateToProps

--- web/src/register/success-without-purchase.js
+++ web/src/register/success-without-purchase.js
@@ -13,8 +13,8 @@ export const RegisterSuccessWithoutPurchase = ({ loading }) =>
 const mapStateToProps = (
   { pending },
   { params: { itemId } }
-) => {
-  return { loading: pending.length > 0 };
-};
+) => ({
+  loading: pending.length > 0
+});
 
 export default connect(mapStateToProps)(RegisterSuccessWithoutPurchase);
