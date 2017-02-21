eba93d5ded775b1f724a6dc8f9f3bf3904e1d8a6
Sam Burnstone <sburnstone@scottlogic.com>
2017-02-21 12:43:29 +0000

    Update store API response to return session data

--- api/src/controllers/store.ts
+++ api/src/controllers/store.ts
@@ -1,14 +1,15 @@
 import { updateUser } from '../../../user/src/client/index';
 import { authenticateAccessToken } from '../middleware/authenticate';
-import { storeCodeToStoreID, storeItems } from '../services/store';
+import { getSessionData } from '../services/session';
+import { storeCodeToStoreID } from '../services/store';
 
 const updateDefaultStoreCode = async (key, userID, storeCode) => {
   return await updateUser(key, userID, { defaultStoreId: storeCodeToStoreID(storeCode) });
 };
 
 const updateStoreAndGetItems = async (key, userId, storeCode) => {
-  await updateDefaultStoreCode(key, userId, storeCode);
-  return storeItems(storeCode);
+  const user = await updateDefaultStoreCode(key, userId, storeCode);
+  return await getSessionData(key, { user });
 };
 
 export default (router) => {
