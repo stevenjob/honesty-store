ffd4df5cb6b2450d9aeade3def527575d80b26fc
Sam Burnstone <sburnstone@scottlogic.com>
2017-02-23 14:14:53 +0000

    Slight tweaks to confirm store change text

--- web/src/confirm-store-change/index.js
+++ web/src/confirm-store-change/index.js
@@ -13,7 +13,7 @@ const ConfirmStoreChange = ({ params: { storeCode }, defaultStoreCode, performSt
   >
     <div>
       <h1>Changing stores?</h1>
-      <p>We've detected that {storeCode} isn't your usual store. At the moment you can only be associated with a single store. Would you like us to move you over to {storeCode} (you can always change your association back on the profile screen)?</p>
+      <p>We've detected that {storeCode} isn't your usual store and at the moment you can only be associated with a single store. Would you like us to move you over to {storeCode} (you can always change your association back on the profile screen)?</p>
       <p><Button onClick={() => performStoreChange({ storeCode })}>Change to {storeCode}</Button></p>
       <p><Button onClick={() => history.push('/store')}>Show {defaultStoreCode}</Button></p>
     </div>
