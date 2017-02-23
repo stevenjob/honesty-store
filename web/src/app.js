22b7d07941c3a8a063330680a07573ff2754996f
Sam Burnstone <sburnstone@scottlogic.com>
2017-02-23 12:38:35 +0000

    Add loading component at App level

--- web/src/app.js
+++ web/src/app.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Error from './chrome/error';
+import Loading from './chrome/loading';
 import { connect } from 'react-redux';
 
-const App = ({ error, children }) =>
-  error ?
-    <Error /> :
-    children;
+const App = ({ error, children, loading }) => {
+  if (error) {
+    return <Error />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+  return children;
+};
 
-const mapStateToProps = ({ error }) => ({ error });
+
+const mapStateToProps = ({ error, pending }) => ({
+  error,
+  loading: pending.length > 0
+});
 
 export default connect(mapStateToProps)(App);
