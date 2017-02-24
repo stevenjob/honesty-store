cf09265f4ad5f932f85567edabdae47eaba2dffb
Chris Price <price.c@gmail.com>
2017-02-24 16:06:34 +0000

    make the store seem more exciting!!!!

--- web/src/store/item.js
+++ web/src/store/item.js
@@ -5,16 +5,23 @@ import Currency from '../format/Currency';
 
 export default ({ item: { id, name, price, image } }) =>
   <Link to={`/item/${id}`} className="btn regular flex navy">
-    <div className="col-3 bg-center bg-no-repeat"
+    <div className="flex-none col-3 bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${safeLookupItemImage(image)})` }}>
       {'\u00a0'}
     </div>
-    <div className="ml2 right-align flex-auto">
-      <h2 className="h4 mt1">
+    <div className="ml2 flex-auto flex flex-column justify-center">
+      <h2 className="h2 mt1 mb1">
         {name}
       </h2>
-      <h2 className="h4 mt1 regular">
-        <Currency amount={price} />
+      <p className="mt1 mb1">
+        <Link to={`/item/${id}`}>More Info</Link>
+      </p>
+    </div>
+    <div className="ml2 flex-none flex flex-column justify-around">
+      <h2 className="mt0 mb0">
+        <Link to={`/item/${id}`} className="block btn btn-primary btn-big">
+          <Currency amount={price} />
+        </Link>
       </h2>
     </div>
   </Link>;
