2bd627df5550b82c5417fdbad0584911d0032239
Chris Price <price.c@gmail.com>
2017-02-24 14:09:34 +0000

    fix dodgy border at the top of lists

--- web/src/chrome/list.js
+++ web/src/chrome/list.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export default ({ data, itemRenderer }) => (
-  <ul className="list-reset my0 px1 bg-white border-bottom border-gray">
+  <ul className="list-reset my0 px1 bg-white border-bottom border-top border-gray">
     {
       data == null ? <p>'No Data'</p> :
         data.map((item, index) => (
           <li key={index}
-            className="border-gray border-top">
+            className={`border-gray ${index > 0 ? 'border-top' : ''}`}>
             <div className="mxn1">
               {itemRenderer(item, index)}
             </div>
