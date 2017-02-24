fadccd032e901610f4290be4fd27cb2c194f130e
Chris Price <price.c@gmail.com>
2017-02-24 16:34:56 +0000

    add useful detail to the store items

--- web/src/store/item.js
+++ web/src/store/item.js
@@ -3,23 +3,25 @@ import { Link } from 'react-router';
 import safeLookupItemImage from '../item/safeLookupItemImage';
 import Currency from '../format/Currency';
 
-export default ({ item: { id, name, price, image } }) =>
+export default ({ item: { id, name, price, image, notes, weight } }) =>
   <Link to={`/item/${id}`} className="btn regular flex navy">
     <div className="flex-none col-3 bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${safeLookupItemImage(image)})` }}>
       {'\u00a0'}
     </div>
     <div className="ml2 flex-auto flex flex-column justify-center">
-      <h2 className="h2 mt1 mb1">
+      <h2 className="h2 mt1 mb0">
         {name}
       </h2>
-      <p className="mt1 mb1">
-        <Link to={`/item/${id}`}>More Info</Link>
+      <p className="mt0 mb1 aqua">
+        {weight}g
+        {'\u00a0'}
+        {notes}
       </p>
     </div>
     <div className="ml2 flex-none flex flex-column justify-around">
       <h2 className="mt0 mb0">
-        <Link to={`/item/${id}`} className="block btn btn-primary btn-big">
+        <Link to={`/item/${id}`} className="block btn btn-primary btn-big btn-narrow">
           <Currency amount={price} />
         </Link>
       </h2>
