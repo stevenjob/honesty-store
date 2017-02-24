5bc2f9698184cc216d2231149750ba37a18c56e5
Chris Price <price.c@gmail.com>
2017-02-24 14:20:27 +0000

    reduce the size of the title and nav

--- web/src/layout/chrome.js
+++ web/src/layout/chrome.js
@@ -16,7 +16,7 @@ const Chrome = ({
         <div className="col-4 left-align">
           {left || '\u00a0'}
         </div>
-        <h1 className="col-4 mt2 mb2 center">
+        <h1 className="col-4 mt2 h3 mb2 center">
           {title || '\u00a0'}
         </h1>
         <div className="col-4 right-align">
@@ -24,16 +24,16 @@ const Chrome = ({
         </div>
       </div>
     </header>
-    <section style={{ paddingTop: '4.5em', paddingBottom: nav ? '4.5em' : '0' }}>
+    <section style={{ paddingTop: '3.5625rem', paddingBottom: nav ? '3.3125rem' : '0' }}>
       {children}
     </section>
     {nav &&
       <nav className="fixed bottom-0 right-0 left-0 bg-silver border-gray border-top white">
         <div className="sm-col-10 md-col-8 lg-col-6 mx-auto flex justify-around">
-          <Store className="inline-block btn p1 center gray regular h6" style={{ width: '6em' }} />
-          <History className="inline-block btn p1 center gray regular h6" style={{ width: '6em' }} />
-          <Profile className="inline-block btn p1 center gray regular h6" style={{ width: '6em' }} />
-          <Help className="inline-block btn p1 center gray regular h6" style={{ width: '6em' }} />
+          <Store className="inline-block btn btn-small line-height-1 center gray regular h6" style={{ width: '4rem' }} />
+          <History className="inline-block btn btn-small line-height-1 center gray regular h6" style={{ width: '4rem' }} />
+          <Profile className="inline-block btn btn-small line-height-1 center gray regular h6" style={{ width: '4rem' }} />
+          <Help className="inline-block btn btn-small line-height-1 center gray regular h6" style={{ width: '4rem' }} />
         </div>
       </nav>
     }
