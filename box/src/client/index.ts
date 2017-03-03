4ef38f8f1983b8f3c2f3fbe168b394af1d3ac260
Sam Burnstone <sburnstone@scottlogic.com>
2017-03-03 12:08:48 +0000

    Update Box type

--- box/src/client/index.ts
+++ box/src/client/index.ts
@@ -1,22 +1,46 @@
 import fetch from '../../../service/src/fetch';
 
-interface BoxItem {
-  itemID: string;
+interface Counted {
   count: number;
   depleted?: number;
 }
+interface Versioned {
+  version: number;
+}
+interface Identified {
+  id: string;
+}
+interface BoxItemWithBatchReference {
+  itemID: string;
+  batches: {
+    id: string;
+    count: number;
+  }[];
+}
+interface BoxItemOverheads {
+  shippingCost: number;
+  warehousingCost: number;
+  packagingCost: number;
+  packingCost: number;
+  serviceFee: number;
+  creditCardFee: number;
+}
+interface BoxShippingDetails {
+  shippingCost: number;
+  packed?: yyyymmdd;
+  shipped?: yyyymmdd;
+  received?: yyyymmdd;
+  closed?: yyyymmdd;
+}
+
+type BoxItem = BoxItemWithBatchReference & Counted & BoxItemOverheads;
 
 type yyyymmdd = string;
 
-export interface Box { // this is duplicated in aws/src/table/table.ts
-  id: string;
-  version: number;
-  boxItems: BoxItem[];
-  packed: yyyymmdd;
-  shipped: yyyymmdd;
-  received: yyyymmdd;
-  closed?: yyyymmdd;
-}
+export type BoxSubmission = BoxShippingDetails & { boxItems: BoxItemWithBatchReference[]; };
+
+// this is duplicated in aws/src/table/table.ts
+export type Box = BoxShippingDetails & Versioned & Identified & { boxItems: BoxItem[]; };
 
 const { get, post } = fetch('box');
 
