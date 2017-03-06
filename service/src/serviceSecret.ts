ce2a0892cf2e84988cee11e0c2520a4c398cd99e
Sam Burnstone <sburnstone@scottlogic.com>
2017-03-06 09:25:56 +0000

    Alter clock tolerance to be 30s

--- service/src/serviceSecret.ts
+++ service/src/serviceSecret.ts
@@ -1,15 +1,14 @@
 import jsonwebtoken = require('jsonwebtoken');
-import ms = require('ms');
 import { baseUrl } from './baseUrl';
 
 const secret = process.env.SERVICE_TOKEN_SECRET;
 if (!secret) {
   throw new Error('no $SERVICE_TOKEN_SECRET provided');
 }
-export const signServiceSecret = () => jsonwebtoken.sign({ baseUrl }, secret, { algorithm: 'HS256', expiresIn: '30s' });
+export const signServiceSecret = () => jsonwebtoken.sign({ baseUrl }, secret, { algorithm: 'HS256', expiresIn: 30 });
 
 export const verifyServiceSecret = (opaqueObject) => {
-  const { baseUrl: foundBaseUrl } = jsonwebtoken.verify(opaqueObject, secret, { algorithms: ['HS256'], clockTolerance: ms('30s') });
+  const { baseUrl: foundBaseUrl } = jsonwebtoken.verify(opaqueObject, secret, { algorithms: ['HS256'], clockTolerance: 30 });
 
   if (foundBaseUrl !== baseUrl) {
     const e: any = new Error('Incorrect service secret');
