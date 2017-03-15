84b9dd2eff87a49efea932b925c2324e081602fd
Sam Burnstone <sburnstone@scottlogic.com>
2017-03-15 12:37:43 +0000

    Expand error detail with redirection url and action descriptions

--- web/src/error/errors.js
+++ web/src/error/errors.js
@@ -1,12 +1,24 @@
 // this is duplicated in typescript @ service/src/error.ts
 
+const retrySignInText = 'Please try signing in again';
+
 export const errorDefinitions = {
-  TopupExceedsMaxBalance: { message: 'Topping up would exceed your maximum balance' },
+  TopupExceedsMaxBalance: {
+    message: 'Topping up would exceed your maximum balance'
+  },
   TooManyPurchaseItems: { message: 'You\'re purchasing too many items' },
   EmailNotFound: { message: 'Couldn\'t find your email' },
-  NoCardDetailsPresent: { message: 'We have no card details for you' },
+  NoCardDetailsPresent: {
+    message: 'We have no card details for you',
+    actionDescription: 'Please contact support',
+    redirectionURL: '/help'
+  },
   EmailTokenInvalid: { message: 'The magic link you followed has expired' },
-  StoreNotFound: { message: 'We couldn\'t find that store code', },
+  StoreNotFound: {
+    message: 'We couldn\'t find that store code',
+    actionDescription: 'Could you double check it and try again?',
+    redirectionURL: '/'
+  },
   LocalStorageBlocked: { message: 'We can\'t hold onto your session in private browsing' },
   NetworkError: { message: 'Sorry, we\'re having trouble connecting' },
   CardIncorrectNumber: { message: 'Incorrect card number' },
@@ -15,11 +27,34 @@ export const errorDefinitions = {
   CardInvalidExpiryYear: { message: 'Invalid expiry year' },
   CardIncorrectCVC: { message: 'Incorrect CVC' },
   CardInvalidCVC: { message: 'Invalid CVC' },
-  CardExpired: { message: 'Card expired' },
-  CardDeclined: { message: 'Card declined' },
+  CardExpired: {
+    message: 'Card expired',
+    actionDescription: 'Please contact support to add a new card',
+    redirectionURL: '/help'
+  },
+  CardDeclined: {
+    message: 'Card declined',
+    actionDescription: 'Please contact support if the problem persists'
+  },
   CardError: { message: 'Hit a problem with your card details' },
-  TokenError: { message: 'Your local session is corrupt - please sign in' },
-  AccessTokenExpired: { message: 'Your access has expired - please sign in' },
-  RefreshTokenExpired: { message: 'Your session has expired - please sign in' },
-  MagicLinkTokenExpired: { message: 'Your magic link token has expired, please try again' }
+  TokenError: {
+    message: 'Your local session is corrupt',
+    actionDescription: retrySignInText,
+    redirectionURL: '/register'
+  },
+  AccessTokenExpired: {
+    message: 'Your access has expired',
+    actionDescription: retrySignInText,
+    redirectionURL: '/register'
+  },
+  RefreshTokenExpired: {
+    message: 'Your session has expired',
+    actionDescription: retrySignInText,
+    redirectionURL: '/register'
+  },
+  MagicLinkTokenExpired: {
+    message: 'Your magic link token has expired',
+    actionDescription: retrySignInText,
+    redirectionURL: '/register'
+  }
 };
