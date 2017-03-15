801fc17fce874ec605dacca48cebe58a3e719657
Sam Burnstone <sburnstone@scottlogic.com>
2017-03-15 16:46:15 +0000

    Rejig layout of error definitions to group internal and external errors

--- web/src/error/errors.js
+++ web/src/error/errors.js
@@ -11,12 +11,22 @@ export const errorDefinitions = {
     message: 'You\'re purchasing too many items',
     actionDescription: ''
   },
-  EmailNotFound: { message: 'Couldn\'t find your email' },
   NoCardDetailsPresent: {
     message: 'We have no card details for you',
     actionDescription: 'Please get in touch with us',
     redirectionURL: '/help/card/no-details',
     dismissalText: 'Tap to get in touch'
+  },
+  CardError: { message: 'Hit a problem with your card details' },
+  CardExpired: {
+    message: 'Card expired',
+    actionDescription: 'Please get in touch with us to add a new card',
+    redirectionURL: '/help/card/expired',
+    dismissalText: 'Tap to get in touch'
+  },
+  CardDeclined: {
+    message: 'Card declined',
+    actionDescription: 'Please get in touch with us if the problem persists'
   },
   StoreNotFound: {
     message: 'We couldn\'t find that store code',
@@ -29,43 +39,20 @@ export const errorDefinitions = {
     actionDescription: 'Please disable it and try again'
   },
   NetworkError: { message: 'Sorry, we\'re having trouble connecting' },
-  CardIncorrectNumber: { message: 'Incorrect card number' },
-  CardInvalidNumber: { message: 'Invalid card number'},
-  CardInvalidExpiryMonth: { message: 'Invalid expiry month' },
-  CardInvalidExpiryYear: { message: 'Invalid expiry year' },
-  CardIncorrectCVC: { message: 'Incorrect CVC' },
-  CardInvalidCVC: { message: 'Invalid CVC' },
-  CardExpired: {
-    message: 'Card expired',
-    actionDescription: 'Please get in touch with us to add a new card',
-    redirectionURL: '/help/card/expired',
-    dismissalText: 'Tap to get in touch'
-  },
-  CardDeclined: {
-    message: 'Card declined',
-    actionDescription: 'Please get in touch with us if the problem persists'
-  },
-  CardError: { message: 'Hit a problem with your card details' },
-  TokenError: {
-    message: 'Your local session is corrupt',
-    actionDescription: retrySignInText,
-    redirectionURL: '/register'
-  },
-  AccessTokenExpired: {
-    message: 'Your access has expired',
-    actionDescription: retrySignInText,
-    redirectionURL: '/register'
-  },
-  RefreshTokenExpired: {
-    message: 'Your session has expired',
-    actionDescription: retrySignInText,
-    redirectionURL: '/register'
-  },
   MagicLinkTokenInvalid: { message: 'The magic link you followed has expired' },
   MagicLinkTokenExpired: {
     message: 'Your magic link token has expired',
     actionDescription: retrySignInText,
     redirectionURL: '/register',
     dismissalText: 'Tap to try again'
-  }
+  },
+  // The following error codes are handled internally and never presented to the user
+  EmailNotFound: { message: 'Couldn\'t find your email' },
+  RefreshTokenExpired: { message: 'Your session has expired' },
+  CardIncorrectNumber: { message: 'Incorrect card number' },
+  CardInvalidNumber: { message: 'Invalid card number'},
+  CardInvalidExpiryMonth: { message: 'Invalid expiry month' },
+  CardInvalidExpiryYear: { message: 'Invalid expiry year' },
+  CardIncorrectCVC: { message: 'Incorrect CVC' },
+  CardInvalidCVC: { message: 'Invalid CVC' },
 };
