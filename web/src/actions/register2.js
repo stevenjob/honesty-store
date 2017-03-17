ae18a0470bd9da6232decc7bd696921ef4a8446e
Sam Burnstone <sburnstone@scottlogic.com>
2017-03-17 12:07:33 +0000

    Pull out code related to stripe token/card validation

--- web/src/actions/register2.js
+++ web/src/actions/register2.js
@@ -1,22 +1,7 @@
 import apifetch from './apirequest';
 import history from '../history';
 
-const createToken = (data) =>
-  new Promise((resolve, reject) => {
-    const stripeResponseHandler = (status, response) => {
-      if (response.error != null) {
-        const error = Object.assign(new Error(), response.error);
-        return reject(error);
-      }
-      if (status !== 200) {
-        return reject(new Error(`Non-200 response ${status} from Stripe`));
-      }
-      resolve(response.id);
-    };
-    const Stripe = window.Stripe;
-    Stripe.setPublishableKey(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
-    Stripe.card.createToken(data, stripeResponseHandler);
-  });
+import { createStripeToken, paramFromCardProviderError } from './stripe-token';
 
 export const REGISTER2_REQUEST = 'REGISTER2_REQUEST';
 export const REGISTER2_SUCESSS = 'REGISTER2_SUCESSS';
@@ -52,42 +37,6 @@ const register2Failure = (error) => {
     type: REGISTER2_FAILURE,
     error
   };
-};
-
-const createStripeToken = ({ number, cvc, exp }) => {
-  const Stripe = window.Stripe;
-  if (!Stripe.card.validateCardNumber(number)) {
-    throw Object.assign(new Error('Invalid card number'), { param: 'number', fromLocalValidation: true });
-  }
-  if (!Stripe.card.validateExpiry(exp)) {
-    throw Object.assign(new Error('Invalid expiry'), { param: 'exp', fromLocalValidation: true });
-  }
-  if (!Stripe.card.validateCVC(cvc)) {
-    throw Object.assign(new Error('Invalid CVC'), { param: 'cvc', fromLocalValidation: true });
-  }
-  return createToken({ number, cvc, exp });
-};
-
-const paramFromCardProviderError = ({ code }) => {
-  switch (code) {
-    case 'CardIncorrectNumber':
-    case 'CardInvalidNumber':
-      return 'number';
-
-    case 'CardInvalidExpiryMonth':
-    case 'CardInvalidExpiryYear':
-    case 'CardExpired':
-      return 'exp';
-
-    case 'CardIncorrectCVC':
-    case 'CardInvalidCVC':
-      return 'cvc';
-
-    case 'CardDeclined':
-    case 'CardError':
-    default:
-      return '';
-  }
 };
 
 export const performRegister2 = ({ itemID, topUpAmount, emailAddress, cardDetails }) => async (dispatch, getState) => {
---
+++ web/src/actions/stripe-token.js
@@ -0,0 +1,52 @@
+const createToken = (data) =>
+  new Promise((resolve, reject) => {
+    const stripeResponseHandler = (status, response) => {
+      if (response.error != null) {
+        const error = Object.assign(new Error(), response.error);
+        return reject(error);
+      }
+      if (status !== 200) {
+        return reject(new Error(`Non-200 response ${status} from Stripe`));
+      }
+      resolve(response.id);
+    };
+    const Stripe = window.Stripe;
+    Stripe.setPublishableKey(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+    Stripe.card.createToken(data, stripeResponseHandler);
+  });
+
+export const createStripeToken = ({ number, cvc, exp }) => {
+  const Stripe = window.Stripe;
+  if (!Stripe.card.validateCardNumber(number)) {
+    throw Object.assign(new Error('Invalid card number'), { param: 'number', fromLocalValidation: true });
+  }
+  if (!Stripe.card.validateExpiry(exp)) {
+    throw Object.assign(new Error('Invalid expiry'), { param: 'exp', fromLocalValidation: true });
+  }
+  if (!Stripe.card.validateCVC(cvc)) {
+    throw Object.assign(new Error('Invalid CVC'), { param: 'cvc', fromLocalValidation: true });
+  }
+  return createToken({ number, cvc, exp });
+};
+
+export const paramFromCardProviderError = ({ code }) => {
+  switch (code) {
+    case 'CardIncorrectNumber':
+    case 'CardInvalidNumber':
+      return 'number';
+
+    case 'CardInvalidExpiryMonth':
+    case 'CardInvalidExpiryYear':
+    case 'CardExpired':
+      return 'exp';
+
+    case 'CardIncorrectCVC':
+    case 'CardInvalidCVC':
+      return 'cvc';
+
+    case 'CardDeclined':
+    case 'CardError':
+    default:
+      return '';
+  }
+};
