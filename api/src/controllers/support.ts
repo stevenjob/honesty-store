f5d5602a552a5fbca824d3993746483e253bf1de
Robert Pilling <rpilling@scottlogic.co.uk>
2017-03-22 16:13:54 +0000

    split out sendSlack{,Support}Message

--- api/src/controllers/support.ts
+++ api/src/controllers/support.ts
@@ -1,13 +1,26 @@
 import fetch from 'node-fetch';
-import { info } from '../../../service/src/log';
-import { authenticateAccessToken } from '../middleware/authenticate';
 import isEmail = require('validator/lib/isEmail');
 import uuid = require('uuid/v4');
 
-const sendSlackSupportMessage = async ({ key, user, message, emailAddress, userAgent }) => {
-  const validatedEmailAddress = user.emailAddress || (isEmail(emailAddress) ? emailAddress : null);
-  const validatedUserAgent = userAgent.substr(0, 250);
+import { Key } from '../../../service/src/key';
+import { info } from '../../../service/src/log';
+import { authenticateAccessToken } from '../middleware/authenticate';
+
+interface SlackMessageParams {
+  key: Key;
+  user: any;
+  message: string;
+  emailAddress?: string;
+  fields: {
+    title: string;
+    value: string;
+  }[];
+}
+
+export const sendSlackMessage = async ({ key, user, message, emailAddress, fields }: SlackMessageParams) => {
+  const validatedEmailAddress = user.emailAddress || (emailAddress && isEmail(emailAddress) ? emailAddress : null);
   const requestId = uuid();
+
   const response = await fetch('https://hooks.slack.com/services/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', {
     method: 'POST',
     headers: {
@@ -40,20 +53,32 @@ const sendSlackSupportMessage = async ({ key, user, message, emailAddress, userA
               title: 'Email',
               value: validatedEmailAddress
             },
-            {
-              title: 'User Agent',
-              value: validatedUserAgent
-            }
+            ...fields
           ]
         }
       ]
     })
   });
-  info(key, 'Support message sent', message, response.status);
+  info(key, 'Support message sent', message, response.status, requestId);
   if (response.status !== 200) {
     throw new Error(`Unexpected response code from slack ${response.status}`);
   }
-  return {};
+};
+
+const sendSlackSupportMessage = async ({ key, user, message, emailAddress, userAgent }) => {
+  const validatedUserAgent = userAgent.substr(0, 250);
+  return await sendSlackMessage({
+    key,
+    user,
+    message,
+    emailAddress,
+    fields: [
+      {
+        title: 'User Agent',
+        value: validatedUserAgent
+      }
+    ]
+  });
 };
 
 export default (router) => {
