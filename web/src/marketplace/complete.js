ad4c80f520d6e552ae035a2524cb9cf8b54bdf5a
Robert Pilling <rpilling@scottlogic.co.uk>
2017-03-27 12:39:32 +0100

    use Success modal on marketplace success screen

--- web/src/marketplace/complete.js
+++ web/src/marketplace/complete.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router';
-import Full from '../layout/full';
-import { Back } from '../chrome/link';
+import { Success } from '../layout/alert';
 
 export default () =>
-  <Full top={<Back />}>
-    <h1>Thanks for contributing to your store!</h1>
-    <p>Your request is awaiting approval, we'll notify you when it is listed.</p>
-    <Link to="/store" className="btn btn-primary btn-big">Back to My Store</Link>
-  </Full>;
+  <Success title="Thanks for contributing to your store!"
+    subtitle="Your request is awaiting approval, we'll notify you when it is listed."
+    onClick={() => history.replace(`/store`)} />;
