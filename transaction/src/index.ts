c237058b8efea4501c7b96b32a36a4f2ed6bc57b
Robert Pilling <rpilling@scottlogic.co.uk>
2017-03-29 07:45:17 +0100

    Revert "fix fetching non-cached transactions"
    This reverts commit 01a433b4762a7bd523bb329904806a79def3fac9.

--- transaction/src/index.ts
+++ transaction/src/index.ts
@@ -22,7 +22,7 @@ const getAccountAndTransactions = async ({ accountId, limit = GET_TRANSACTION_LI
     : transactionHead;
 
   const transactions = idToFetch ? await getTransactions({
-    transactionId: idToFetch,
+    transactionId: createTransactionId({ accountId, transactionId: idToFetch }),
     limit: limit - cachedTransactions.length
   }) : [];
 
