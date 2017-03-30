d88b433a13eba7767f89394c1e1280dd34121408
Sam Burnstone <sburnstone@scottlogic.com>
2017-03-30 08:33:26 +0100

    Import history to enable dismissal from success page

--- web/src/marketplace/complete.js
+++ web/src/marketplace/complete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Success } from '../layout/alert';
+import history from '../history';
 
 export default () =>
   <Success title="Thanks for contributing to your store!"
