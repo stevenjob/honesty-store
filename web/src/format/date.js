91a26edf70f20c54e870d83d95dc9551af765708
Sam Burnstone <sburnstone@scottlogic.com>
2017-04-19 15:03:13 +0100

    cater for getMonth() being zero-based

--- web/src/format/date.js
+++ web/src/format/date.js
@@ -3,6 +3,6 @@ import zeroPad from '../format/number';
 export default (timestamp) => {
   const date = new Date(timestamp);
   const dayInMonth = zeroPad(date.getDate());
-  const month = zeroPad(date.getMonth());
+  const month = zeroPad(date.getMonth() + 1);
   return `${dayInMonth}/${month}/${date.getFullYear()}`;
 };
