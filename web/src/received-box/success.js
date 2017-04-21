ba7c5ddf3df26a03ac4dae078c20ae5d8b3d1cdd
Sam Burnstone <sburnstone@scottlogic.com>
2017-04-21 12:33:25 +0100

    adjust text of box received success page

--- web/src/received-box/success.js
+++ web/src/received-box/success.js
@@ -2,6 +2,6 @@ import React from 'react';
 import history from '../history';
 import { Success } from '../layout/alert';
 
-export default () => <Success title="The items will appear in the store shortly, but feel free to put the box out right-away"
+export default () => <Success title="The items should now be available in your store"
   subtitle="Hooray, your box has arrived!"
   onClick={() => history.replace(`/store`)} />;
