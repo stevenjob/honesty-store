a69352b9969bd657acf160af761d6bf01686034d
Sam Burnstone <sburnstone@scottlogic.com>
2017-04-24 16:42:55 +0100

    Move box-submit script over to using commander

--- scripts/src/box-submit-hs.ts
+++ scripts/src/box-submit-hs.ts
@@ -1,18 +1,8 @@
+// tslint:disable:no-console
+import * as program from 'commander';
 import { readFileSync } from 'fs';
-
-import { BoxItemWithBatchReference, createShippedBox, ShippedBoxSubmission } from '../../box/src/client';
+import { BoxItemWithBatchReference, createShippedBox, markBoxAsShipped, ShippedBoxSubmission } from '../../box/src/client';
 import { createServiceKey } from '../../service/src/key';
-
-const usage = (help): never => {
-  console.error('Usage: path/to/script shipping-cost store-id dry-run');
-  console.error('reads (from standard input) lines consisting of:');
-  console.error('  count item-id batch-id');
-  console.error(help);
-  // tslint:disable-next-line:no-constant-condition
-  while (true) {
-    process.exit(2);
-  }
-};
 
 const throwParseError = (type, name, str) => {
   throw new Error(`${name} not a ${type} (${str})`);
@@ -34,13 +24,14 @@ const maybeParseInt = (str, name) => {
   throwParseError('number', name, str);
 };
 
-const main = async (argv) => {
-  if (argv.length !== 3) { usage('wrong number of arguments'); }
+const warnAndExit = e => {
+  console.error(e);
+  process.exit(1);
+};
 
-  const shippingCost = maybeParseInt(argv[0], 'shipping-cost');
-  const storeId = argv[1];
-  const isDryRun = maybeParseBool(argv[2], 'dryrun');
+const key = createServiceKey({ service: 'marketplace-script' });
 
+const packBox = async (shippingCost: number, storeId: string, isDryRun: boolean, date: number) => {
   const boxItems = readFileSync('/dev/stdin')
     .toString()
     .split('\n')
@@ -82,24 +73,49 @@ const main = async (argv) => {
     }, new Map<string, BoxItemWithBatchReference>())
     .values();
 
-  const key = createServiceKey({ service: 'marketplace-script' });
-
   const submission: ShippedBoxSubmission = {
     donationRate: 0,
     boxItems: Array.from(boxItems),
     shippingCost,
-    packed: null,
-    shipped: null
+    packed: date
   };
 
   const response = await createShippedBox(key, storeId, submission, isDryRun);
-  // tslint:disable-next-line:no-console
   console.log(JSON.stringify(response, null, 2));
 };
 
-main(process.argv.slice(2))
-  .then(() => void 0)
-  .catch((e) => {
-    console.error(e.message);
-    process.exit(1);
+const shipBox = async (boxId: string, date: number) => markBoxAsShipped(key, boxId, date);
+
+program.command('pack <shipping-cost> <store-id> <dry-run>')
+  // tslint:disable-next-line:max-line-length
+  .description(`Submits a box with 'packed' field set to the current date (can be overriden using '-d' option). Reads box data (from standard input) lines consisting of: count item-id batch-id`)
+  .option('-d, --date [date]', 'date box was packed, as a unix timestamp')
+  .action((shippingCost, storeId, isDryRun, options) =>
+    packBox(
+      maybeParseInt(shippingCost, 'shipping-cost'),
+      storeId,
+      maybeParseBool(isDryRun, 'dry-run'),
+      options.date ? maybeParseInt(options.date, 'date') : Date.now()
+    ).catch(warnAndExit)
+  );
+
+program.command('ship <box-id>')
+  .description(`Updates the 'shipped' field of the box with the supplied id, set to current date (can be overriden using '-d' option)`)
+  .option('-d, --date [date]', 'date box was shipped, as a unix timestamp')
+  .action((boxId, options) =>
+    shipBox(
+      boxId,
+      options.date ? maybeParseInt(options.date, 'date') : Date.now()
+    ).catch(warnAndExit)
+  );
+
+program.command('*')
+  .action(() => {
+    program.help();
   });
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+  program.help();
+}
