5efd5e005d9353c1206f726d0b12f62a08fb3b95
Robert Pilling <rpilling@scottlogic.co.uk>
2017-05-10 14:58:05 +0100

    prefer "== null" over undefined

--- service/src/baseUrl.ts
+++ service/src/baseUrl.ts
@@ -1,13 +1,13 @@
 export const baseUrl = process.env.BASE_URL;
 
 // tslint:disable-next-line:triple-equals
-if (baseUrl == undefined) {
+if (baseUrl == null) {
   throw new Error('no $BASE_URL provided');
 }
 
 export const lambdaBaseUrl = process.env.LAMBDA_BASE_URL;
 
 // tslint:disable-next-line:triple-equals
-if (lambdaBaseUrl == undefined) {
+if (lambdaBaseUrl == null) {
   throw new Error('no $LAMBDA_BASE_URL provided');
 }
