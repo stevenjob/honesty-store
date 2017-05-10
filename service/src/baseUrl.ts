4a2b2138f694d9c8033fd50c387de499e8d9798f
Robert Pilling <rpilling@scottlogic.co.uk>
2017-05-10 10:53:47 +0100

    improve environment variable checks

--- service/src/baseUrl.ts
+++ service/src/baseUrl.ts
@@ -1,11 +1,13 @@
 export const baseUrl = process.env.BASE_URL;
 
-if (!baseUrl) {
+// tslint:disable-next-line:triple-equals
+if (baseUrl == undefined) {
   throw new Error('no $BASE_URL provided');
 }
 
 export const lambdaBaseUrl = process.env.LAMBDA_BASE_URL;
 
-if (!lambdaBaseUrl) {
+// tslint:disable-next-line:triple-equals
+if (lambdaBaseUrl == undefined) {
   throw new Error('no $LAMBDA_BASE_URL provided');
 }
