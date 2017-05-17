25cd58f753e7a54a9774bfed8b01593df5a02c0d
Sam Burnstone <sburnstone@scottlogic.com>
2017-05-17 11:31:15 +0100

    transform legacy storeCode to storeId when getting transaction

--- transaction/src/transaction.ts
+++ transaction/src/transaction.ts
@@ -55,6 +55,30 @@ export const assertValidTransaction = ({ type, amount, data, id }: Transaction) 
   }
 };
 
+const externalisePurchaseTransactionData = (data) => {
+  const { storeId: existingStoreId } = data;
+
+  if (existingStoreId == null || isUUID(existingStoreId)) {
+    return data;
+  }
+
+  const legacyStoreCodeToId = {
+    'sl-edn': 'f79ff70c-2103-43f9-922d-d54a16315361',
+    'sl-ncl': 'b8d7305b-bb7d-4bbe-8b2f-5e94c6267bb6',
+    'sl-ldn': '1cfb21d8-52d8-4eee-98ce-740c466bfc0e',
+    'sl-brs': '9a61dad3-f05c-46aa-a7e4-14311e9cccc5',
+    'dev-test': '1e7c9c0d-a9be-4ab7-8499-e57bf859978d'
+  };
+  const updatedStoreId = legacyStoreCodeToId[existingStoreId];
+  if (updatedStoreId == null) {
+    throw new Error(`Unexpected legacy storeId found: ${existingStoreId}`);
+  }
+  return {
+    ...data,
+    storeId: updatedStoreId
+  };
+};
+
 const getTransaction = async (id) => {
   const response = await new DynamoDB.DocumentClient()
     .get({
@@ -68,7 +92,15 @@ const getTransaction = async (id) => {
     throw new Error(`Transaction not found ${id}`);
   }
 
-  return <InternalTransaction>item;
+  const transaction = <InternalTransaction>item;
+
+  if (transaction.type === 'purchase') {
+    return {
+      ...transaction,
+      data: externalisePurchaseTransactionData(transaction.data)
+    };
+  }
+  return transaction;
 };
 
 export const getTransactions = async ({ transactionId, limit = Infinity }): Promise<Transaction[]> => {
