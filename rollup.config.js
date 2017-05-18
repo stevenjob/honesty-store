3921ec74d45536215008eb88ac0a11fcaa613920
Chris Price <price.c@gmail.com>
2017-05-18 18:32:17 +0100

    replace ignore with a custom plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import builtinModules from 'builtin-modules';
-import ignore from 'rollup-plugin-ignore';
 import json from 'rollup-plugin-json';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
 const externalModules = [...builtinModules, 'aws-sdk'];
+
+const hacksPrefix = '\0hacks:';
+const hackedModules = {
+  'moment': 'export default { }',
+  'pkginfo': 'export default function() { }',
+  'winston': 'export default { Logger: function() {} }'
+};
 
 const options = {
   entry: 'lib/src/lambda.js',
@@ -16,11 +22,32 @@ const options = {
       name: 'resolve-external',
       resolveId: (name) => options.external(name) ? name : null
     },
+    {
+      name: 'hacks',
+      resolveId: (name) => {
+        const hackModule = Object.keys(hackedModules)
+          .some(key => new RegExp('(^|:)' + key + '$').test(name));
+        if (hackModule) {
+          return hacksPrefix + name;
+        }
+      },
+      load: (id) => {
+        if (!new RegExp(hacksPrefix).test(id)) {
+          return;
+        }
+        const parts = id.split(':');
+        const name = parts[parts.length - 1];
+        const value = hackedModules[name];
+        if (!value) {
+          throw new Error('Unknown module ' + name);
+        }
+        return value;
+      }
+    },
     resolve({
       preferBuiltins: true
     }),
     commonjs(),
-    ignore(['moment']),
     json()
   ],
   external: name => externalModules.indexOf(name) > -1
