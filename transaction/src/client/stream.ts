175f4aa7ee6df39572cba07d50186e044e99d60e
Robert Pilling <rpilling@scottlogic.co.uk>
2017-05-30 17:41:28 +0100

    replace switch statement with isTransaction()

--- transaction/src/client/stream.ts
+++ transaction/src/client/stream.ts
@@ -7,15 +7,8 @@ export const subscribeTransactions = function* (event) {
   for (const record of event.Records) {
     const converted = DynamoDB.Converter.output({ M: record.dynamodb.NewImage });
 
-    switch (converted.type) {
-      case 'topup':
-      case 'purchase':
-        yield <Transaction>converted;
-        break;
-
-      default:
-        // an account
-        break;
+    if (isTransaction(converted.type)) {
+      yield <Transaction>converted;
     }
   }
 };
