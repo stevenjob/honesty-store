5b20ebf5d9cc323ec3897f3fe80c8e883516becb
Robert Pilling <rpilling@scottlogic.co.uk>
2017-05-30 16:26:36 +0100

    show a (short) txId and transaction amount in slack

--- transaction-slack/src/lambda.ts
+++ transaction-slack/src/lambda.ts
@@ -6,7 +6,7 @@ import { Transaction } from '@honesty-store/transaction/src/client';
 import { subscribeTransactions } from '@honesty-store/transaction/src/client/stream';
 import { getUser } from '@honesty-store/user/src/client';
 
-const recordPurchase = async (key, { type, data: { userId, itemId, storeId } }: Transaction) => {
+const recordPurchase = async (key, { id: transactionId, type, data: { userId, itemId, storeId }, amount }: Transaction) => {
   if (type !== 'purchase') {
     return;
   }
@@ -14,10 +14,12 @@ const recordPurchase = async (key, { type, data: { userId, itemId, storeId } }: 
   const user = await getUser(key, userId);
   const item = await getItem(key, itemId);
   const store = await getStoreFromId(key, storeId);
+  const shortTxId = transactionId.replace(/^(.{5}).*:(.{5}).*/, '$1:$2');
 
   await sendSlackMessageOneLine({
     key,
-    message: `${user.emailAddress} bought "${item.name}" from ${store.code}`,
+    // tslint:disable-next-line:max-line-length
+    message: `${user.emailAddress} bought "${item.name} ${item.qualifier}" from ${store.code} @ £${(-amount / 100).toFixed(2)} (\`${shortTxId}\`)`,
     channel: 'purchases'
   });
 };
