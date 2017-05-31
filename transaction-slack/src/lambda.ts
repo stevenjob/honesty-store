33f749b24439fffeae1f0f59c946f30851195ef5
Robert Pilling <rpilling@scottlogic.co.uk>
2017-05-31 14:50:01 +0100

    post slack messages as csv

--- transaction-slack/src/lambda.ts
+++ transaction-slack/src/lambda.ts
@@ -1,3 +1,5 @@
+import { stringify as csvStringifyCb } from 'csv';
+
 import { getItem } from '@honesty-store/item/src/client';
 import { createServiceKey } from '@honesty-store/service/src/key';
 import { sendSlackMessageOneLine } from '@honesty-store/service/src/slack';
@@ -5,6 +7,15 @@ import { getStoreFromId } from '@honesty-store/store/src/client';
 import { Transaction } from '@honesty-store/transaction/src/client';
 import { subscribeTransactions } from '@honesty-store/transaction/src/client/stream';
 import { getUser } from '@honesty-store/user/src/client';
+
+const csvStringify = (objects, options) =>
+  new Promise((resolve, reject) =>
+    csvStringifyCb(objects, options, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    }));
 
 const recordPurchase = async (key, { id: transactionId, type, data: { userId, itemId, storeId }, amount }: Transaction) => {
   if (type !== 'purchase') {
@@ -16,10 +27,22 @@ const recordPurchase = async (key, { id: transactionId, type, data: { userId, it
   const store = await getStoreFromId(key, storeId);
   const shortTxId = transactionId.replace(/^(.{5}).*:(.{5}).*/, '$1:$2');
 
+  const csvMessage = await csvStringify(
+    [{
+      emailAddress: user.emailAddress,
+      itemDescription: `${item.name} ${item.qualifier}`,
+      store: store.code,
+      price: -amount,
+      shortTxId
+    }],
+    {
+      header: false
+    });
+
   await sendSlackMessageOneLine({
     key,
     // tslint:disable-next-line:max-line-length
-    message: `${user.emailAddress} bought "${item.name} ${item.qualifier}" from ${store.code} @ £${(-amount / 100).toFixed(2)} (\`${shortTxId}\`)`,
+    message: csvMessage,
     channel: 'purchases'
   });
 };
