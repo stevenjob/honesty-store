4863cb5be4260bb9dc5ee71255ec847ee1dfcc07
Chris Price <price.c@gmail.com>
2017-06-08 18:03:22 +0100

    add assert methods for validation of objects

--- service/src/assert.ts
+++ service/src/assert.ts
@@ -6,3 +6,47 @@ export const createAssertValidUuid = (name) =>
       throw new Error(`Invalid ${name} ${uuid}`);
     }
   };
+
+export const assertValidUuid = (key: string, value: any) => {
+  if (value == null || !isUUID(value, 4)) {
+    throw new Error(`Invalid ${key} ${value}`);
+  }
+};
+
+export const assertValidString = (key: string, value: any) => {
+  if (typeof value !== 'string') {
+    throw new Error(`Invalid ${key} ${value}`);
+  }
+};
+
+export const assertPositiveInteger = (key: string, value: any) => {
+  if (!Number.isInteger(value) || value < 0) {
+    throw new Error(`Invalid ${key} ${value}`);
+  }
+};
+
+export const assertNonZeroPositiveInteger = (key: string, value: any) => {
+  if (!Number.isInteger(value) || value <= 0) {
+    throw new Error(`Invalid ${key} ${value}`);
+  }
+};
+
+type Validator = (key: string, value: any) => void;
+
+type ObjectValidator<Type> = { [Key in keyof Type]: Validator };
+
+export const createAssertValidObject = <Type>(validator: ObjectValidator<Type>) =>
+  (object: Type) => {
+    for (const key of <(keyof Type)[]>Object.keys(object)) {
+      const value = object[key];
+      const keyValidator = validator[key];
+      if (keyValidator == null) {
+        throw new Error(`Invalid ${key} ${value}`);
+      }
+      validator[key](key, value);
+    }
+  };
+
+export const assertNever = (value: never): never => {
+  throw new Error(`Invalid value ${value}`);
+};
