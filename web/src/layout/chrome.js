6e2763b06d5f5b4d1ef8477601a59bca1bd11dd5
Robert Pilling <rpilling@scottlogic.co.uk>
2017-06-12 11:29:19 +0100

    move nav to top and remove icons

--- web/src/layout/chrome.js
+++ web/src/layout/chrome.js
@@ -1,57 +1,74 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Store, History, Profile, Help } from './nav';
-import isRegisteredUser from '../reducers/is-registered-user.js';
+import { Link, withRouter } from 'react-router';
 
-const Chrome = ({ title, left, right, children, nav }) => (
-  <div className="sm-col-10 md-col-8 lg-col-6 mx-auto">
-    <header className="fixed top-0 right-0 left-0 bg-navy border-silver border-bottom white z1">
-      <div className="sm-col-10 md-col-8 lg-col-6 mx-auto flex items-center ">
-        <div className="col-4 left-align">
-          {left || '\u00a0'}
+import isRegisteredUser from '../reducers/is-registered-user.js';
+import logo from '../chrome/assets/logo-inverted.svg';
+
+const navBgColor = "#29526d";
+const navFgColorActive = "#ffffff";
+const navFgColorInactive = "#7591a1";
+const navBarColorActive = "#3fbcd5";
+
+const NavLink = withRouter(({ to, className, text, router }) => (
+  <Link to={to}>
+    <div
+      className={`${className} center inline-block`}
+      style={{
+        background: navBgColor,
+        borderBottomWidth: '0.2rem',
+        borderBottomStyle: 'solid',
+        borderBottomColor: router.isActive(to) ? navBarColorActive : "#ffffff",
+        color: router.isActive(to) ? navFgColorActive : navFgColorInactive
+      }}
+    >
+      {text}
+    </div>
+  </Link>
+));
+
+const imageHeight = '4rem';
+
+const Chrome = ({ title: _title, storeCode, children, showNav }) => (
+  <div className="col-12">
+    <div className="fixed top-0 right-0 left-0 bg-navy border-silver white z1">
+      <header>
+        <div className="mx-auto">
+          <div className="mx4 mt3">
+            <img src={logo} alt="honesty.store" style={{ maxHeight: imageHeight, minHeight: imageHeight }} />
+          </div>
+
+          <h3 className="my2 center regular">
+            {storeCode || '\u00a0'}
+          </h3>
         </div>
-        <h1 className="col-4 mt2 h3 mb2 center">
-          {title || '\u00a0'}
-        </h1>
-        <div className="col-4 right-align">
-          {right || '\u00a0'}
-        </div>
-      </div>
-    </header>
+      </header>
+
+      {
+        showNav &&
+        <nav>
+          <div className="col-12">
+            <NavLink className="py2 col-4" to="/store" text="Store" />
+            <NavLink className="py2 col-4" to="/profile" text="Profile" />
+            <NavLink className="py2 col-4" to="/help" text="Support" />
+          </div>
+        </nav>
+      }
+    </div>
+
     <section
       style={{
-        paddingTop: '3.5625rem',
-        paddingBottom: nav ? '3.3125rem' : '0'
+        paddingTop: showNav ? '13.6rem' : '10rem'
       }}
     >
       {children}
     </section>
-    {nav &&
-      <nav className="fixed bottom-0 right-0 left-0 bg-silver border-gray border-top white">
-        <div className="sm-col-10 md-col-8 lg-col-6 mx-auto flex justify-around">
-          <Store
-            className="inline-block btn btn-small line-height-1 center gray regular h6"
-            style={{ width: '4rem' }}
-          />
-          <History
-            className="inline-block btn btn-small line-height-1 center gray regular h6"
-            style={{ width: '4rem' }}
-          />
-          <Profile
-            className="inline-block btn btn-small line-height-1 center gray regular h6"
-            style={{ width: '4rem' }}
-          />
-          <Help
-            className="inline-block btn btn-small line-height-1 center gray regular h6"
-            style={{ width: '4rem' }}
-          />
-        </div>
-      </nav>}
   </div>
 );
 
-const mapStateToProps = ({ user }) => ({
-  nav: isRegisteredUser(user)
+const mapStateToProps = ({ user, store: { code: storeCode } }) => ({
+  storeCode,
+  showNav: isRegisteredUser(user)
 });
 
 export default connect(mapStateToProps, {})(Chrome);
