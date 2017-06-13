7d1ba38385035d779b799014bf7f87e9fc89e902
Sam Burnstone <sburnstone@scottlogic.com>
2017-06-13 16:17:50 +0100

    remove circular dependency

--- store/src/client/index.ts
+++ store/src/client/index.ts
@@ -1,6 +1,5 @@
 import fetch from '@honesty-store/service/src/fetch';
 import { Transaction } from '@honesty-store/transaction/src/client';
-import isUUID = require('validator/lib/isUUID');
 
 export interface StoreItemDetails {
   name: string;
@@ -94,22 +93,3 @@ export const updateItemCount = (key, storeId: string, itemId: string, count: num
 
 export const unlistItem = (key, storeId: string, itemId: string) =>
   post<Store>(1, key, `/${storeId}/${itemId}/unlist`, {});
-
-export const migrateStoreCodeToId = storeCodeOrId => {
-  if (isUUID(storeCodeOrId)) {
-    return storeCodeOrId;
-  }
-
-  const migration = {
-    'sl-edn': 'f79ff70c-2103-43f9-922d-d54a16315361',
-    'sl-ncl': 'b8d7305b-bb7d-4bbe-8b2f-5e94c6267bb6',
-    'sl-ldn': '1cfb21d8-52d8-4eee-98ce-740c466bfc0e',
-    'sl-brs': '9a61dad3-f05c-46aa-a7e4-14311e9cccc5',
-    'dev-test': '1e7c9c0d-a9be-4ab7-8499-e57bf859978d'
-  };
-  const id = migration[storeCodeOrId];
-  if (!id) {
-    throw new Error(`Couldn't migrate store code '${storeCodeOrId}'`);
-  }
-  return id;
-};
