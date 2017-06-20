6bc3dcd9b2e89324a849a99e21d39138e015883f
Robert Pilling <rpilling@scottlogic.co.uk>
2017-06-20 16:18:18 +0100

    update and rename custom-topup script

--- support/src/custom-fund.ts
+++ support/src/custom-fund.ts
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
 
 import { createUserKey } from '@honesty-store/service/lib/key';
-import { createTransaction } from '@honesty-store/transaction';
+import { createTransaction, transactionTypes } from '@honesty-store/transaction';
 import { getUser } from '@honesty-store/user';
 
 const usage = () => {
   const script = process.argv[1];
   const [, filename = script] = script.match(/\/([^/]+)$/);
 
-  console.error(`Usage: ${filename} userId amount extra-json-data`);
+  console.error(`Usage: ${filename} userId type amount extra-json-data`);
+  console.error(`  type should be one of ${transactionTypes.join(', ')}`);
+  console.error(``);
   console.error(`  $SERVICE_TOKEN_SECRET and $BASE_URL required in the environment`);
   console.error(`  $BASE_URL should be https://live.honesty.store for real topups, avoiding the https://honesty.store CDN`);
   console.error(``);
   // tslint:disable-next-line:max-line-length
-  console.error(`  e.g. SERVICE_TOKEN_SECRET=service:xNxznoraYHUq+A0O33OJ23pwwfQAtK12SCt BASE_URL=https://honesty.store ${filename} 56b60280-5823-4058-9b5b-24f4c2660876 500  '{ "scottcoinTx": "<scottcoin-block-hash>" }'`);
+  console.error(`  e.g. SERVICE_TOKEN_SECRET=... BASE_URL=... ${filename} 56b60280-5823-4058-9b5b-24f4c2660876 credit 500 '{ "scottcoinTx": "<scottcoin-block-hash>" }'`);
   process.exit(2);
 };
 
 const main = async (args) => {
-  if (args.length !== 3) {
+  if (args.length !== 4) {
     usage();
   }
 
-  const [ userId, amountArgument, jsonArgument ] = args;
+  const [ userId, type, amountArgument, jsonArgument ] = args;
 
   const amount = Number(amountArgument);
   const json = JSON.parse(jsonArgument);
@@ -35,7 +37,7 @@ const main = async (args) => {
     correlationKey,
     user.accountId,
     {
-      type: 'topup',
+      type,
       amount,
       data: json
     });
