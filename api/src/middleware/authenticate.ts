990c2d4f7ce72fb4686a84172f4b14b16d5f925a
Sam Burnstone <sburnstone@scottlogic.com>
2017-06-27 15:10:00 +0100

    make authentication error more generic

--- api/src/middleware/authenticate.ts
+++ api/src/middleware/authenticate.ts
@@ -62,7 +62,7 @@ const authenticateAdminUser = (request, _response, next) => {
     'c71733c4-dc05-42f9-848e-fb53bf08a2d7'
   ];
   if (!allowedUserIds.some((el) => el === id)) {
-    throw new CodedError('AccessDenied', `userId ${id} does not have permission to view item details`);
+    throw new CodedError('AccessDenied', `userId ${id} does not have permission`);
   }
   next();
 };
