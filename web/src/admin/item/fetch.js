85817aae67007212e6c2b564afde5ceb3add942f
Sam Burnstone <sburnstone@scottlogic.com>
2017-06-27 17:24:09 +0100

    remove unneeded checking of state

--- web/src/admin/item/fetch.js
+++ web/src/admin/item/fetch.js
@@ -3,25 +3,15 @@ import { connect } from 'react-redux';
 import { performAllItems } from '../../actions/all-items';
 
 class ItemFetch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      valid: false
-    };
-  }
   componentWillMount() {
     const { items, pending, performAllItems } = this.props;
     if (items == null && !pending.some(el => el === 'all-items')) {
       performAllItems();
-    } else {
-      this.setState({
-        valid: true
-      });
     }
   }
 
   render() {
-    return this.state.valid ? this.props.children : null;
+    return this.props.children;
   }
 }
 
