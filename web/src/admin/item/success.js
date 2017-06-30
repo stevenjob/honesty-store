27b51f0fa9711ba28011ea6b55d4e31ecda1f4f5
Sam Burnstone <sburnstone@scottlogic.com>
2017-06-30 14:56:36 +0100

    redirect to all item list on successful item addition

--- web/src/admin/item/success.js
+++ web/src/admin/item/success.js
@@ -5,6 +5,6 @@ import { Success } from '../../layout/alert';
 export default ({ params: { itemId } }) => (
   <Success
     title="Item details successfully stored"
-    onClick={() => history.replace(`/admin/item/${itemId}`)}
+    onClick={() => history.replace(`/admin/item`)}
   />
 );
