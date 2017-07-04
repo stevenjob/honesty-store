6ec09b319e76bddef71d5450ce0c8fe181747e8c
Sam Burnstone <sburnstone@scottlogic.com>
2017-07-04 14:28:48 +0100

    remove history from nav and set profile text to current balance

--- web/src/layout/chrome.js
+++ web/src/layout/chrome.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router';
 
 import * as Logo from '../chrome/logo';
-import Balance from '../topup/balance';
+import Currency from '../format/Currency';
 import isRegisteredUser from '../reducers/is-registered-user';
 
 const navBarHeight = '3.4rem';
@@ -26,22 +26,19 @@ const NavLink = withRouter(({ to, className, router, children }) => (
 
 const Chrome = ({ storeCode, children, registered, balance }) => (
   <div className="col-12 sm-col-10 md-col-8 lg-col-6 mx-auto">
-    <header className="bg-navy center pt4 pb2 relative">
+    <header className="bg-navy center pt2 pb2 relative">
       <div className="top-0 right-0 m1 white absolute">
-        {registered
-          ? <Balance balance={balance} />
-          : <Link className="btn btn-big white" to="/register">Sign In</Link>}
+        {!registered &&
+          <Link className="btn btn-big white" to="/register">Sign In</Link>}
       </div>
-      <Logo.Inverted className="col-9 py1" />
+      <Logo.Inverted className="col-9 mb1 mt3" />
       <h2 className="my0 white regular">{storeCode}</h2>
     </header>
     <div style={{ minHeight: '100vh' }}>
       <nav className="sticky top-0 bg-navy white flex items-center z1">
         <NavLink className="flex-auto" to="/store">Store</NavLink>
         {registered &&
-          <NavLink className="flex-auto" to="/profile">Profile</NavLink>}
-        {registered &&
-          <NavLink className="flex-auto" to="/history">History</NavLink>}
+          <NavLink className="flex-auto" to="/profile"><Currency amount={balance} /></NavLink>}
         <NavLink className="flex-auto" to="/help">Help</NavLink>
       </nav>
 
