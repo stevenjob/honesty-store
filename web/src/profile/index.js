f9ac9f7fa8982f1be6b920c33e91b6a1e255066a
Sam Burnstone <sburnstone@scottlogic.com>
2017-07-04 15:58:16 +0100

    link to topup and show correct balance

--- web/src/profile/index.js
+++ web/src/profile/index.js
@@ -4,13 +4,16 @@ import { Link } from 'react-router';
 import Chrome from '../layout/chrome';
 import StoreBrowser from '../chrome/store-browser';
 import { performStoreChange } from '../actions/store';
+import Currency from '../format/Currency';
 
 const Profile = ({ emailAddress, balance, storeCode, performStoreChange }) => (
   <Chrome>
     <div className="navy bg-white center py2">
       <h3 className="mt0 mb1 regular">Your balance is</h3>
-      <h1 className="mt1 mb1 regular">£5.00</h1>
-      <h3 className="regular my0 regular"><Link>Topup</Link></h3>
+      <h1 className="mt1 mb1 regular"><Currency amount={balance} /></h1>
+      <h3 className="regular my0 regular">
+        <Link to="/topup/500">Topup</Link>
+      </h3>
     </div>
     <div className="navy bg-white p2 mt1 col-12">
       <div className="flex border-bottom border-silver">
