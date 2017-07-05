e4213d524d2b56aae3eafb3ec9e6ebf78e1198b0
Sam Burnstone <sburnstone@scottlogic.com>
2017-07-05 16:00:40 +0100

    show 'Profile' as nav title when profile route is active

--- web/src/layout/chrome.js
+++ web/src/layout/chrome.js
@@ -8,21 +8,23 @@ import isRegisteredUser from '../reducers/is-registered-user';
 
 const navBarHeight = '3.4rem';
 
-const NavLink = withRouter(({ to, className, router, children }) => (
-  <Link
-    to={to}
-    className={`${className} center block border-bottom border-white gray`}
-    activeClassName="border-aqua"
-    activeStyle={{ color: 'white' }}
-    style={{
-      borderBottomWidth: '0.2rem',
-      lineHeight: navBarHeight
-    }}
-    onlyActiveOnIndex={true}
-  >
-    {children}
-  </Link>
-));
+const NavLink = withRouter(
+  ({ to, className, router, activeText, children }) => (
+    <Link
+      to={to}
+      className={`${className} center block border-bottom border-white gray`}
+      activeClassName="border-aqua"
+      activeStyle={{ color: 'white' }}
+      style={{
+        borderBottomWidth: '0.2rem',
+        lineHeight: navBarHeight
+      }}
+      onlyActiveOnIndex={true}
+    >
+      {router.isActive(to) ? activeText || children : children}
+    </Link>
+  )
+);
 
 const Chrome = ({ storeCode, children, registered, balance }) => (
   <div className="col-12 sm-col-10 md-col-8 lg-col-6 mx-auto">
@@ -38,7 +40,7 @@ const Chrome = ({ storeCode, children, registered, balance }) => (
       <nav className="sticky top-0 bg-navy white flex items-center z1">
         <NavLink className="flex-auto" to="/store">Store</NavLink>
         {registered &&
-          <NavLink className="flex-auto" to="/profile">
+          <NavLink className="flex-auto" to="/profile" activeText="Profile">
             <Currency amount={balance} />
           </NavLink>}
         <NavLink className="flex-auto" to="/help">Help</NavLink>
