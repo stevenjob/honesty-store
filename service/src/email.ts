04db972bff00cc473435cadd61dac070a3a75b89
Robert Pilling <rpilling@scottlogic.co.uk>
2017-07-10 17:19:19 +0100

    add service/src/email.ts

---
+++ service/src/email.ts
@@ -0,0 +1,27 @@
+import { config, SES } from 'aws-sdk';
+
+config.region = process.env.AWS_REGION;
+
+type EmailMessage = {
+  to: string[];
+  from: string;
+  subject: string;
+  message: string;
+};
+
+export const sendEmail = async ({ to, from, subject, message }: EmailMessage) => {
+  const response = await new SES({ apiVersion: '2010-12-01' })
+    .sendEmail({
+      Destination: {
+        ToAddresses: to
+      },
+      Source: from,
+      Message: {
+        Subject: { Charset: 'UTF-8', Data: subject },
+        Body: { Text: { Charset: 'UTF-8', Data: message } }
+      }
+    })
+    .promise();
+
+  return response.MessageId;
+};
