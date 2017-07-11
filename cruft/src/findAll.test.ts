d33ba925612d5e8a77e44c2370939b07a3f7373d
Chris Price <price.c@gmail.com>
2017-07-11 09:34:16 +0100

    reduce page count to fix timeout

--- cruft/src/findAll.test.ts
+++ cruft/src/findAll.test.ts
@@ -28,14 +28,14 @@ describe.only(suiteName, () => {
 
   it('should find items over many pages', async () => {
     const id = nextId();
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 10; i++) {
       await cruft.create({ id: `${id}_${i}`, version: 0, bar: id });
     }
     let i = 0;
     for await (const _item of cruft.findAll({ bar: id })) {
       i++;
     }
-    expect(i).to.equal(15, 'Incorrect item count');
+    expect(i).to.equal(10, 'Incorrect item count');
   });
 
   it('should find items over many pages and stop if break called', async () => {
