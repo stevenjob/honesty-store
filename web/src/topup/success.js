125444a52211b307362e02facdcba8d1dfc914d8
Robert Pilling <rpilling@scottlogic.co.uk>
2017-07-11 14:24:11 +0100

    change topup-success message for non-prepay

--- web/src/topup/success.js
+++ web/src/topup/success.js
@@ -3,16 +3,18 @@ import { connect } from 'react-redux';
 import history from '../history';
 import { Success } from '../layout/alert';
 import Currency from '../format/Currency';
+import { determinePrepay } from '../balance/prepay';
 
-export const TopupSuccess = ({ balance }) => (
+export const TopupSuccess = ({ balance, isPrepay }) => (
   <Success
     title={<span>Your balance is now <Currency amount={balance} /></span>}
-    subtitle="Thank you for your top up!"
+    subtitle={isPrepay ? "Thank you for your top up!" : "Thank you for updating your card!"}
     onClick={() => history.replace(`/profile/history`)}
   />
 );
 
-const mapStateToProps = ({ user: { balance = 0 } }) => ({
+const mapStateToProps = ({ user: { balance = 0, creditLimit } }) => ({
+  isPrepay: determinePrepay(creditLimit),
   balance
 });
 
