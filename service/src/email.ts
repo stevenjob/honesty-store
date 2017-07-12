72c275e3d0950c9a2eae8a7454ae47c75ca86150
Robert Pilling <rpilling@scottlogic.co.uk>
2017-07-12 11:26:56 +0100

    add replyTo in sendEmail()

--- service/src/email.ts
+++ service/src/email.ts
@@ -5,18 +5,20 @@ config.region = process.env.AWS_REGION;
 type EmailMessage = {
   to: string[];
   from?: string;
+  replyTo?: string[];
   subject: string;
   message: string;
 };
 
 const defaultFrom = 'no-reply@honesty.store';
 
-export const sendEmail = async ({ to, from = defaultFrom, subject, message }: EmailMessage) => {
+export const sendEmail = async ({ to, from = defaultFrom, replyTo, subject, message }: EmailMessage) => {
   const response = await new SES({ apiVersion: '2010-12-01' })
     .sendEmail({
       Destination: {
         ToAddresses: to
       },
+      ReplyToAddresses: replyTo,
       Source: from,
       Message: {
         Subject: { Charset: 'UTF-8', Data: subject },
