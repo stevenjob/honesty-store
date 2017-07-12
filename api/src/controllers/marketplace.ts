32adf51818c5923801ee9881681ad78e87ab9f37
Robert Pilling <rpilling@scottlogic.co.uk>
2017-07-12 11:33:17 +0100

    improve marketplace email message

--- api/src/controllers/marketplace.ts
+++ api/src/controllers/marketplace.ts
@@ -13,11 +13,20 @@ export default (router) => {
     ) => {
       const subject = 'Marketplace Request';
 
+      const adminUrl = `https://honesty.store/admin/listing/${storeCode}`;
+
+      const message = `A marketplace request has been made - if you approve of this, you can list the item(s) at ${adminUrl}.
+Replying to this email will send your message to the user who submitted the request.
+
+See the below "Item" fields for more details.
+`;
+
       return await mailStoreAgent({
         key,
         storeCode,
         fromUser: user,
         subject,
+        message,
         fields:  [
           {
             title: 'Item-Description',
